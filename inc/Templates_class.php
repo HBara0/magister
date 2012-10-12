356197796837d41dc8e370b08f4353113dba6c4f
zaher.reda <zaher.reda@orkila.com>
2012-10-12 15:33:20 +0300

    Changes to the templates file based system
    1. Template files are now under ./inc/templates
    2. Index.html file in ./inc/templates
    3. Changes in class to
    3.1 Default is DB
    3.2 Sanitize path and different path selection

--- inc/Templates_class.php
+++ inc/Templates_class.php
@@ -14,21 +14,20 @@ class Templates {
 	public function get($title, $raw=1) {
 		global $db;
 
-		//if(!isset($this->cache[$title]))
-		//{
-			if (TEMPLATES_SYSTEM=='DB') {
-				$template_content = $db->fetch_field($db->query("SELECT template FROM ".Tprefix."templates WHERE title='".$db->escape_string($title)."'"), 'template');
-			}
-			else {
-				$base_dir=substr(ROOT,0,strlen($base_dir)-1).'\templates';
-				$templatefile = $base_dir.'\\'.$title;
+		if(!isset($this->cache[$title])) {
+			if(TEMPLATES_SYSTEM == 'FILE') {
+				//$base_dir = ROOT.INC_ROOT.'templates';//substr(ROOT, 0, strlen($base_dir)-1).'\templates';
+				$templatefile = $core->sanitize_path(ROOT.INC_ROOT.'templates/'.$title.'.tpl');
 				$handle = fopen($templatefile, 'r');
 				$template_content = fread($handle, filesize($templatefile));
 				fclose($handle);
 			}
-
+			else 
+			{
+				$template_content = $db->fetch_field($db->query("SELECT template FROM ".Tprefix."templates WHERE title='".$db->escape_string($title)."'"), 'template');
+			}
 			$this->cache[$title] = $template_content;
-		//}
+		}
 		$template = "<!-- start: {$title} -->\n".$this->cache[$title]."\n<!-- end: {$title} -->";
 
 		if($raw == 1) {
