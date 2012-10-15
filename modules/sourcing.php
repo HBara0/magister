ba574b05ebe7ffda6b08061e9e76f1dcd84ffd1f
tony.assaad <tony.assaad@orkila.com>
2012-10-15 11:21:41 +0300

    supplier profile

--- modules/sourcing.php
+++ modules/sourcing.php
@@ -1,6 +1,6 @@
 <?php
 $module['name'] = 'sourcing';
-$module['title'] = 'SOURCING';
+$module['title'] = 'Sourcing';
 $module['homepage'] = 'managesupplier';
 $module['globalpermission'] = 'canUseSourcing';
 $module['menu'] = array('file' 		  => array('managesupplier','listpotentialsupplier'),
