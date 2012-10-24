0d4af6e1590e063b1c484c77ccfcd667aa519a5e
zaher.reda <zaher.reda@orkila.com>
2012-10-24 08:51:12 +0300

    Update to jQuery 1.8.2 and jQuery UI 1.9.0 with custom theme roll

--- js/jscript.js
+++ js/jscript.js
@@ -3,7 +3,7 @@ $(function() {
 		Check the browswer version before anything else
 	*/
 	if($.browser.msie && $.browser.version.substr(0,1)<7) {
-		$('head').append('<link rel="stylesheet" href="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.css" type="text/css" />');
+		$('head').append('<link rel="stylesheet" href="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.min.css" type="text/css" />');
 		$("body").append("<div id='browserversionerror' title='Browser version is too old'>Please upgrade your browser to a newer version.</div>");
 		$("#browserversionerror").dialog({
 			bgiframe: true,
@@ -62,12 +62,6 @@ $(function() {
 		$("#sidedesignImage").height($(window).height());
 	}
 
-	if($("input[id^='pickDate']").length > 0) {
-		if($('link[id="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.css"') == 'undefined') {
-			$('head').append('<link rel="stylesheet" href="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.css" type="text/css" />');
-		}
-	}
-
 	if($(".texteditormin, .texteditor").length > 0) {
 		//if($('link[id="' + rootdir + 'js/redactor.min.js"') == 'undefined') {
 			$('head').append('<script src="' + rootdir + 'js/redactor.min.js" type="text/javascript"></script>');
@@ -88,6 +82,7 @@ $(function() {
 			if((e.which == 102 || e.which == 70) && e.ctrlKey) {
 				e.preventDefault();
 				$('#tablefilters, .tablefilters_row').toggle();
+				$('html, body').animate({ scrollTop: 0 }, 'fast');
 				$('#tablefilters').find('input:first').focus();
 				return false;
 			}
@@ -329,7 +324,7 @@ $(function() {
 	});
 
 	function show_loginbox() {
-		$('head').append('<link rel="stylesheet" href="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.css" type="text/css" />');
+		$('head').append('<link rel="stylesheet" href="' + rootdir + 'css/jqueryuitheme/jquery-ui-current.custom.min.css" type="text/css" />');
 		popUp("", "popup_loginbox", "users.php");
 	}
 	
