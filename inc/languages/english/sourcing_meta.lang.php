dbe75c682f425ae487ca86afe070bb07d9716594
tony.assaad <tony.assaad@orkila.com>
2012-12-06 10:23:29 +0200

    1- email Verification For Notification Upon Chemical Request
    2-now the RML will be shown  in the supplier profile .
    3-adjusting preview of the contact person tooltip  to show the Notes and the phone

--- inc/languages/english/sourcing_meta.lang.php
+++ inc/languages/english/sourcing_meta.lang.php
@@ -39,6 +39,7 @@ $lang['competitor'] = 'Competitor';
 $lang['contactdesc'] = 'Notes';
 $lang['website'] = 'Website';
 $lang['chemicalname'] = 'Chemical Name';
+$lang['chemicalsearch'] = 'Search By Chemical';
 $lang['chemicalproducts'] = 'Chemical Products';
 $lang['comments'] = 'Comments And Reviews';
 $lang['requestchemical'] = 'Request Chemical';
