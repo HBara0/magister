dec0e82247c0ba036bcd15c096af356f8dc25ddd
tony.assaad <tony.assaad@orkila.com>
2013-03-20 10:27:34 +0200

    commit conflicts files

--- inc/Sessions_class.php
+++ inc/Sessions_class.php
@@ -7,149 +7,150 @@
  * $id: Sessions_class.php
  * Last Update: @zaher.reda 	August 30, 2012 | 12:27 PM
  */
-
 class Sessions {
 	public $sid, $uid, $ipaddress;
-
+	
 	public function __construct() {
 		global $db, $core;
 		$this->cleanup();
-
+		
 		if(!isset($core->cookies['sid'])) {
-			$this->create();
+			$this->create(); 
 		}
-		else {
+		else
+		{
 			$this->update(); 
 		}
-
+		
 		if($this->uid != 0) {
-			$user = new Users($this->uid, FALSE);
-			if($user) {
-				$core->user = $user->get();
+			$query = $db->query("SELECT * FROM ".Tprefix."users WHERE uid='".$this->uid."'");		
+			if($db->num_rows($query) > 0) {
+				$core->user = $db->fetch_assoc($query);
 				unset($core->user['password'], $core->user['salt']);
 				
 				$query2 = $db->query("SELECT * FROM ".Tprefix."usergroups WHERE gid='".$core->user['gid']."'");
 				$core->usergroup = $db->fetch_assoc($query2);
-
-				/* $auditing = $db->query("SELECT eid FROM ".Tprefix."suppliersaudits WHERE uid='".$this->uid."'");
-				  if($db->num_rows($auditing) > 0) {
-				  while($auditfor = $db->fetch_assoc($auditing)) {
-				  $core->user['auditfor'][] = $auditfor['eid'];
-				  }
-				  }
-
-				  if($core->usergroup['canViewAllAff'] == 0) {
-				  $affiliates = get_specificdata("affiliatedemployees", "affid", "affid", "affid", '', 0, "uid='".$this->uid."'");
-				  if(!is_array($affiliates)) {
-				  $suppliers = array(0);
-				  }
-				  $core->user['affiliates'] = $affiliates;
-
-				  if(is_array($core->user['auditfor'])) {
-				  foreach($core->user['auditfor'] as $key => $val) {
-				  $core->user['auditedaffiliates'][$val] = get_specificdata("affiliatedentities", "affid", "affid", "affid", '', 0, "eid='{$val}'");
-				  }
-				  }
-
-				  }
-				 */
-				/* $suppliers = get_specificdata("assignedemployees", "eid", "eid", "eid", '', 0, "uid='".$this->uid."'");
-				  if(!is_array($suppliers)) {
-				  $suppliers = array(0);
-				  }
-				  $core->user['suppliers'] = $suppliers; */
-
-				/* if($core->usergroup['canViewAllCust'] == 0 || $core->usergroup['canViewAllSupp'] == 0) {
-				  $entities = $db->query("SELECT ae.eid, ae.affid, e.type FROM ".Tprefix."assignedemployees ae LEFT JOIN ".Tprefix."entities e ON (e.eid=ae.eid) WHERE ae.uid='".$this->uid."'");
-				  if($db->num_rows($entities) > 0) {
-				  while($entity = $db->fetch_assoc($entities)) {
-				  if($entity['type'] == 's') {
-				  $core->user['suppliers']['eid'][] = $entity['eid'];
-				  $core->user['suppliers']['affid'][$entity['eid']][] = $entity['affid'];
-				  }
-				  elseif($entity['type'] == 'c') {
-				  $core->user['customers'][] = $entity['eid'];
-				  }
-				  }
-				  }
-				  else
-				  {
-				  $core->user['suppliers'] = array(0);
-				  $core->user['customers'] = array(0);
-				  }
-				  } */
-				$core->user += get_user_business_assignments($this->uid); //parse_userentities_data($this->uid);
-
-				if(!isset($core->user['mainaffiliate'])) {
-					$core->user['mainaffiliate'] = $user->get_mainaffiliate()->get()['affid'];
+				
+				/*$auditing = $db->query("SELECT eid FROM ".Tprefix."suppliersaudits WHERE uid='".$this->uid."'");
+				if($db->num_rows($auditing) > 0) {
+					while($auditfor = $db->fetch_assoc($auditing)) {
+						$core->user['auditfor'][] = $auditfor['eid'];
+					}
 				}
+				
+				if($core->usergroup['canViewAllAff'] == 0) {
+					$affiliates = get_specificdata("affiliatedemployees", "affid", "affid", "affid", '', 0, "uid='".$this->uid."'");
+					if(!is_array($affiliates)) {
+						$suppliers = array(0);
+					}
+					$core->user['affiliates'] = $affiliates;
+					
+					if(is_array($core->user['auditfor'])) {
+						foreach($core->user['auditfor'] as $key => $val) {
+							$core->user['auditedaffiliates'][$val] = get_specificdata("affiliatedentities", "affid", "affid", "affid", '', 0, "eid='{$val}'");
+						}
+					}
+					
+				}
+				*/
+				/*$suppliers = get_specificdata("assignedemployees", "eid", "eid", "eid", '', 0, "uid='".$this->uid."'");
+				if(!is_array($suppliers)) {
+					$suppliers = array(0);
+				}
+				$core->user['suppliers'] = $suppliers; */
+
+				/*if($core->usergroup['canViewAllCust'] == 0 || $core->usergroup['canViewAllSupp'] == 0) {
+					$entities = $db->query("SELECT ae.eid, ae.affid, e.type FROM ".Tprefix."assignedemployees ae LEFT JOIN ".Tprefix."entities e ON (e.eid=ae.eid) WHERE ae.uid='".$this->uid."'");
+					if($db->num_rows($entities) > 0) {
+						while($entity = $db->fetch_assoc($entities)) {
+							if($entity['type'] == 's') {
+								$core->user['suppliers']['eid'][] = $entity['eid'];
+								$core->user['suppliers']['affid'][$entity['eid']][] = $entity['affid'];
+							}
+							elseif($entity['type'] == 'c') {
+								$core->user['customers'][] = $entity['eid'];
+							}
+						}
+					}
+					else
+					{
+						$core->user['suppliers'] = array(0);
+						$core->user['customers'] = array(0);
+					}
+				}*/
+				$core->user += get_user_business_assignments($this->uid);//parse_userentities_data($this->uid);
+				
+				if(!isset($core->user['mainaffiliate'])) {
+					$core->user['mainaffiliate'] = $db->fetch_field($db->query("SELECT affid FROM ".Tprefix."affiliatedemployees WHERE uid='{$this->uid}'"), 'affid');
+				}
+				//print_r($core->user);
 			}
 		}
 	}
-
+	
 	protected function cleanup() {
 		global $db, $core;
-
-		$limit = TIME_NOW - (60 * $core->settings['idletime']);
+		
+		$limit = TIME_NOW - (60*$core->settings['idletime']);
 		$db->delete_query('sessions', "time<$limit");
 	}
-
+	
 	protected function create() {
 		global $db, $core;
-
+		
 		$this->sid = md5(uniqid(microtime()));
 		$this->uid = 0;
-		$this->ipaddress = userip();
-
+		$this->ipaddress =  userip();
+		
 		$this->create_dbsession();
-
-		$this->create_cookie('sid', $this->sid, (TIME_NOW + (60 * $core->settings['idletime'])));
-		$this->create_cookie('uid', $this->uid, (TIME_NOW + (60 * $core->settings['idletime'])));
+		
+		$this->create_cookie('sid', $this->sid, (TIME_NOW + (60*$core->settings['idletime'])));
+		$this->create_cookie('uid', $this->uid, (TIME_NOW + (60*$core->settings['idletime'])));
 	}
-
+	
 	protected function update() {
 		global $db, $core;
 		$this->cleanup();
-
+		
 		$this->sid = $db->escape_string($core->cookies['sid']);
 		$this->uid = $db->escape_string($core->cookies['uid']);
-		$this->ipaddress = userip();
-
+		$this->ipaddress =  userip();
+	
 		$this->authenticate_cookie();
-
+		
 		$session_data = array(
-				'uid' => $this->uid
+		'uid'	=> $this->uid
 		);
 
 		$session_information = $db->fetch_assoc($db->query("SELECT uid, time, ip FROM ".Tprefix."sessions WHERE sid='".$this->sid."' ORDER BY time DESC LIMIT 0,1"));
 		if($session_information['uid'] > 0) {
-			/* if($session_information['ip'] != $this->ipaddress) {
-			  unset($core->user);
-			  $db->delete_query('sessions', "sid='".$this->sid."'");
-			  $this->create_cookie('sid', '', (TIME_NOW - 3600));
-			  $this->create_cookie('uid', '', (TIME_NOW - 3600));
-			  $this->create_cookie('loginKey', '', (TIME_NOW - 3600));
-			  $this->uid = 0;
-
-			  return false;
-			  } */
-
-			if((TIME_NOW - $session_information['time']) > (60 * 5)) {
+			/*if($session_information['ip'] != $this->ipaddress) {
+				unset($core->user);
+				$db->delete_query('sessions', "sid='".$this->sid."'");
+				$this->create_cookie('sid', '', (TIME_NOW - 3600));
+				$this->create_cookie('uid', '', (TIME_NOW - 3600));
+				$this->create_cookie('loginKey', '', (TIME_NOW - 3600));
+				$this->uid = 0;
+	
+				return false;
+			}*/
+			
+			if((TIME_NOW - $session_information['time']) > (60*5)) { 
 				$db->update_query("users", array('lastVisit' => TIME_NOW), "uid='".$this->uid."'");
 				$db->update_query('sessions', array('time' => TIME_NOW), "sid='".$this->sid."'");
-				$this->create_cookie('sid', $this->sid, (TIME_NOW + (60 * $core->settings['idletime']))); //60*SESSION_EXPIRE
-				$this->create_cookie('uid', $this->uid, (TIME_NOW + (60 * $core->settings['idletime'])));
+				$this->create_cookie('sid', $this->sid, (TIME_NOW + (60*$core->settings['idletime'])));//60*SESSION_EXPIRE
+				$this->create_cookie('uid', $this->uid, (TIME_NOW + (60*$core->settings['idletime'])));
 			}
 		}
-
+		
 		if($this->uid != $session_information['uid']) {
 			$query = $db->update_query('sessions', $session_data, "sid='".$this->sid."'");
 			//if($db->affected_rows() == 0) { 
-			//$this->create_dbsession();
+				//$this->create_dbsession();
 			//}
 		}
 	}
-
+	
 	private function authenticate_cookie() {
 		global $core, $db;
 		if(empty($core->cookies['loginKey'])) {
@@ -158,7 +159,8 @@ class Sessions {
 			//$this->create(); 
 			return false;
 		}
-		else {
+		else
+		{
 			if(empty($this->uid) || $core->cookies['loginKey'] != $db->fetch_field($db->query("SELECT loginKey FROM ".Tprefix."users WHERE uid={$this->uid}"), 'loginKey')) {
 				unset($core->user);
 				$this->uid = 0;
@@ -166,38 +168,37 @@ class Sessions {
 			}
 		}
 	}
-
+	
 	private function create_dbsession() {
 		global $db;
-
+		
 		$session_data = array(
-				'sid' => $this->sid,
-				'uid' => $this->uid,
-				'time' => TIME_NOW,
-				'ip' => $this->ipaddress
+		'sid'	=> $this->sid,
+		'uid'	=> $this->uid,
+		'time'   => TIME_NOW,
+		'ip'	 => $this->ipaddress
 		);
 		$db->insert_query('sessions', $session_data);
 	}
-
-	public function name_phpsession($name = '') {
+	
+	public function name_phpsession($name='') {
 		return session_name($name);
 	}
-
-	public function id_phpsession($id = '') {
+	
+	public function id_phpsession($id='') {
 		return session_id($id);
 	}
-
+	
 	public function regenerate_id_phpsession($delete_old = false) {
-		return session_regenerate_id($delete_old);
+		return session_regenerate_id($delete_old);	
 	}
-
+	
 	public function start_phpsession() {
 		global $core;
-
-		session_set_cookie_params((TIME_NOW + (60 * $core->settings['idletime'])), COOKIE_PATH, COOKIE_DOMAIN);
+		session_set_cookie_params((TIME_NOW + (60*$core->settings['idletime'])), COOKIE_PATH, COOKIE_DOMAIN);
 		session_start();
 	}
-
+	
 	public function set_phpsession(array $values) {
 		if(is_array($values)) {
 			foreach($values as $key => $val) {
@@ -205,24 +206,24 @@ class Sessions {
 			}
 		}
 	}
-
+	
 	public function get_phpsession($index) {
 		return $_SESSION[$index];
 	}
-
+	
 	public function isset_phpsession($index) {
 		return isset($_SESSION[$index]);
 	}
-
-	public function destroy_phpsession($fully = true) {
+	
+	public function destroy_phpsession($fully=true) {
 		if($fully === true) {
 			if(isset($core->cookies[$this->name_phpsession()])) {
-				$this->create_cookie($this->name_phpsession(), '', TIME_NOW - 42000);
+				$this->create_cookie($this->name_phpsession(), '', TIME_NOW-42000);
 			}
 		}
 		session_destroy();
 	}
-
+	
 	public function generate_token() {
 		$this->token = md5(uniqid(microtime(), true));
 		return $this->token;
@@ -230,32 +231,32 @@ class Sessions {
 
 	public function is_validtoken() {
 		global $core;
-
+		
 		if(empty($this->token)) {
-			$this->token = $this->get_phpsession('token');
+			$this->token = $this->get_phpsession('token');		
 		}
-
+		
 		if(empty($this->token) || $core->input['token'] != $this->token) {
 			return false;
 		}
-
+		
 		return true;
 	}
-
-	protected function create_cookie($name, $value = '', $expire = NULL, $secure = false, $httponly = false) {
+	
+	protected function create_cookie($name, $value='', $expire=NULL, $secure=false, $httponly=false) {
 		if(!defined(COOKIE_PATH)) {
 			define(COOKIE_PATH, '/');
 		}
-
+		
 		if($expire == -1 || $expire === NULL) {
 			$expire = 0;
 		}
-		else {
+		else
+		{
 			$expire += TIME_NOW;
-		}
-
+		}		
+		
 		setcookie(COOKIE_PREFIX.$name, $value, $expire, COOKIE_PATH, COOKIE_DOMAIN, $secure, $httponly);
 	}
-
 }
 ?>
