a0a0037663267297c1acfe96db6e6f9fd2dae095
tony.assaad <tony.assaad@orkila.com>
2013-05-09 15:19:46 +0300

    minor modifications in the matching tool,
    swap the main products in the temporary table  with the right selected  chemical name

--- inc/templates/sourcing_matchproductsstage.tpl
+++ inc/templates/sourcing_matchproductsstage.tpl
@@ -12,13 +12,13 @@
         <td class="contentContainer">
             <h3>{$matchinproducts}</h3>
 
-            <form name="perform_sourcing/matchproduct_Form" action="index.php?module=sourcing/matchproducts" method="post"  id="perform_sourcing/matchproduct_Form">
-
+            <form name="perform_sourcing/matchproducts_Form" action="{$_SERVER[QUERY_STRING]}" method="post"  id="perform_sourcing/matchproducts_Form">
+    <input class="rounded" type="hidden" value="do_match" name="action" id="action" />
                 <div align="center">
                     <table width="100%" class="datatable">
                         <thead>
                         <th width="40%"> <h3>Matchitem</h3>{$lang->matchitem}</th><th>&nbsp;</th><th width="40%"><h3>Matchwith</h3>{$lang->matchwith}</th>
-                 
+
                         </thead>
                         <tbody>
                             {$matchproduct}
@@ -26,9 +26,11 @@
                     </table>
 
                 </div>
-                <input class="rounded" type="hidden" value="do_match" name="action" id="action" />
-                <input type="submit" class="button" value="import" id="perform_sourcing/matchproducts_Button" />
-
+            
+                <input type="button" class="button" value="import" id="perform_sourcing/matchproducts_Button" />
+                <div style="display:table-row">
+                    <div style="display:table-cell;"id="perform_sourcing/matchproducts_Results"></div>
+                </div>
             </form>
         </td>
 
--- modules/sourcing/matchproducts.php
+++ modules/sourcing/matchproducts.php
@@ -22,12 +22,12 @@ function get_mainproducts($id = '') {
 	global $db;
 	if(!empty($id)) {
 
-		for($i = 1; $i <= 30; $i++) {
+		for($i = 1; $i <= 3; $i++) {
 			if($i == 1) {
 				$mainproduct_field .=" Mainproducts".$i."";
 			}
 			else {
-				$mainproduct_field .=", Mainproducts".$i."";
+				$mainproduct_field .=",Mainproducts".$i."";
 			}
 		}
 		$mainproductsquery = $db->query("SELECT  ".$mainproduct_field." FROM importtemp WHERE  id= ".$id." ");
@@ -46,38 +46,39 @@ function matchWithChemical($chemical_id, $product) {
 
 	if($db->num_rows($checkmatch_query) > 0) {
 		//$matchproduct .='<td><select id=matchwith['.$match['csid'].'] name= matchwith['.$match['csid'].'] size="1" tabindex="2">';
-		$matchproduct .= '<option value="0">&nbsp;</option>';
+
 		while($match = $db->fetch_assoc($checkmatch_query)) {
 			$matchproduct = '';
-			$matchproduct .='<td> <select id=matchwith['.$match['csid'].'] name= matchwith['.$match['csid'].'] size="1" tabindex="2">';
+			$matchproduct .='<td><b>'.$chemical_id.' <select id="matchwith['.$chemical_id.']" name= "matchwith['.$chemical_id.']" size="1" tabindex="2">';
 			$option_selected = 'selected="selected"';
-			$matchproduct .= '<option value="'.$match['csid'].'"'.$option_selected.'>'.$match['matchchemical'].'</option>';
+			$matchproduct .= '<option value=""></option>';
+			$matchproduct .= '<option value="'.$match['matchchemical'].'"'.$option_selected.'>'.$match['matchchemical'].'</option>';
 		}
 		$matchproduct .= '</select></td>';
 	}
 	else {
-		$matchproduct = '<td><input id="chemicalproducts_'.$chemical_id.'_QSearch" value="" autocomplete="off" size="40px" type="text">
-						<input id="chemicalproducts_'.$chemical_id.'_id" name="matchwith['.$match['csid'].']" value="" type="hidden">
+		$matchproduct = '<td><input id="chemicalproducts_'.$chemical_id.'_QSearch" value="" autocomplete="off" size="40px" type="text" name="matchwith['.$chemical_id.']">
+						<input id="chemicalproducts_'.$chemical_id.'_id"  value="" type="hidden">
 						<div id="searchQuickResults_chemicalproducts_'.$chemical_id.'" class="searchQuickResults" style="display:none;"></div></td>';
 	}
 	return $matchproduct;
 }
 
 if(!$core->input['action']) {
-
+	$rowid = 1;
 	$mainproducts = get_importtemp_data();
 	foreach($mainproducts as $companyid => $products) {
-
 		foreach($products as $product) {
+			if(empty($product)) {
+				continue;
+			}
 			$matchproduct.='<tr>';
 			if(isset($product) && !empty($product)) {
-				$matchproduct.='<td>#'.$companyid.'<select id=matchproduct['.$companyid.'] name= matchproduct['.$companyid.'] size="1" tabindex="1">';
-				$matchproduct.='<option value="'.$product.'"'.$option_selected.'>'.$product.'</option>';
-				//$matchproduct.='<td>#'.$companyid.'   '.parse_selectlist('matchproduct['.$companyid.']', 1, $products, 1).'</td>';
-				$matchproduct.='</select>';
+				$matchproduct.='<td><b>'.$rowid.' </b><input type="hidden" id="matchproduct['.$rowid.']" name=" matchproduct['.$rowid.']" size="1" tabindex="1"  value="'.$product.'">'.$product;
 				$matchproduct.='<td>&lt;-&gt;</td>';
-				$matchproduct.= matchWithChemical(rand(1, 100), $product);
+				$matchproduct.= matchWithChemical($rowid, $product);
 			}
+			$rowid++;
 		}
 		$matchproduct.='</tr>';
 	}
@@ -86,11 +87,19 @@ if(!$core->input['action']) {
 	output_page($matchproductsstage);
 }
 elseif($core->input['action'] == 'do_match') {
-
-	foreach($core->input['matchproduct'] as $chem) {
-		print_r($chem);
-		echo'<br>';
+	if(is_array($core->input['matchproduct'])) {
+		foreach($core->input['matchproduct'] as $id => $mainproduct) {
+			//swipe the product name
+			if(isset($core->input['matchwith'][$id]) && !empty($core->input['matchwith'][$id])) {
+//				echo '<b>mainproduct : </b> '.($mainproduct).' match --->>>>'.$core->input['matchwith'][$id].' <br> ';
+//				echo'<br>';
+				for($i = 1; $i <= 30; $i++) {
+					$db->update_query("importtemp", array('Mainproducts'.$i => $core->input['matchwith'][$id]), 'Mainproducts'.$i.''.'="'.$db->escape_string($core->input['matchproduct'][$id]).'"');
+				}
+			}
+		}
 	}
+	output_xml("<status>true</status><message>Successfully Saved</message>");
 }
 ?>
 
