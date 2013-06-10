4c71a19e6f3f3a8aca0413e1591ced1ac07df135
zaher.reda <zaher.reda@orkila.com>
2013-06-10 10:35:52 +0300

    Corrected permissions in Assets module config file

--- modules/assets.php
+++ modules/assets.php
@@ -2,10 +2,10 @@
 $module['name'] = 'assets';
 $module['title'] = $lang->assetsmodule;
 $module['homepage'] = 'assets';
-$module['globalpermission'] = 'assets_canTrack';
+$module['globalpermission'] = 'canUseAssets';
 $module['menu'] = array('file' 		  => array('assets&action=list','assets&action=map','manageassets','managetrackers','assignassets'),
 						'title'		 => array('assetstrackpage','assetstrackmap','assetsmanagepage','trackersmanagepage','assignassetspage'),
-						'permission'	=> array('assets_canTrack','assets_canTrack','assets_canManageAssets','assets_canManageAssets','assets_canManageAssets')
+						'permission'	=> array('assets_canTrackAssets','assets_canTrackAssets','assets_canManageAssets','assets_canManageAssets','assets_canManageAssets')
 						);
 
 ?>
