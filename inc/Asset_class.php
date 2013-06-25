3e51de137a5297100adba47efc3c986a74903bd3
tony.assaad <tony.assaad@orkila.com>
2013-06-25 11:57:06 +0300

    restored  modified assets class

--- inc/Asset_class.php
+++ inc/Asset_class.php
@@ -6,17 +6,8 @@
  * Maps Class
  * $id: Maps_class.php
  * Created:		@Alain.Paulikevitch		May 10, 2012 | 06:03 PM
- * Last Update: @Alain.Paulikevitch 	November  29, 2012 | 03:32 PM
+ * Last Update: @tony.assaad	        june 25, 2013 | 01:32 PM
  */
-
-// ignore all users with gid 7 - done
-// fix image button into input type image - done
-// in asset management page add icon that opens the map with the asset
-// list of assets in asset positions list
-// put asset management functions into the class - done
-// put text in html into lang (forms in all pages) - done
-// calendar set of colors for polylines
-// add class databatable to all tables - done by setting width:100%
 
 class Asset {
 	private $cache = array();
@@ -40,6 +31,7 @@ class Asset {
 		if(is_array($data)) {
 			$this->assets = $data;
 		}
+
 		/* Santize inputs - START */
 		$sanitize_fields = array('title', 'affid', 'type', 'description');
 		foreach($sanitize_fields as $val) {
@@ -72,7 +64,7 @@ class Asset {
 	}
 
 	public function set_asid($asid) {
-		$this->my_asid = $asid;
+		$my_asid = $asid;
 	}
 
 	public function record_location($data) {
@@ -116,91 +108,13 @@ class Asset {
 
 	/* Getter Functions ----START */
 	public function get_asid() {
-		return $this->my_asid;
-	}
-
-	public function edit_asset($id, $asset) {
-		global $db;
-		$db->update_query('assets', $asset, 'asid='.$id);
-	}
-
-	public function add_asset($asset) {
-		global $db;
-		$db->insert_query('assets', $asset);
-	}
-
-	public function delete_asset($id) {
-		global $db,$core;
-		if($core->usergroup['assets_canDeleteAsset'] == 1) {
-			//echo 'deleted asset '.$id.'<br>';
-			$db->query('delete from '.Tprefix.'assets where asid='.$id);
-		}
-	}
-
-	public function edit_tracker($id, $tracker) {
-		global $db;
-		$db->update_query('assets_trackingdevices', $tracker, 'atdid='.$id);
-	}
-
-	public function add_tracker($tracker) {
-		global $db;
-		$db->insert_query('assets_trackingdevices', $tracker);
-	}
-
-	public function delete_tracker($id) {
-		global $db, $core;
-		if($core->usergroup['assets_canDeleteTracker'] == 1) {
-			$result = $db->query('select atdid,asid,fromDate,toDate from '.Tprefix.'assets_trackingdevices where atdid='.$id);
-			if($db->num_rows($result) > 0) {
-				if($row = $db->fetch_assoc($result)) {
-					if (TIME_NOW>$row['toDate']) {
-						//echo 'deleted tracker '.$id.'<br>';
-						$db->query('delete from '.Tprefix.'assets_trackingdevices where atdid='.$id);
-					}
-				}
-			}
-		}
-	}
-
-	public function edit_assignedasset($id, $assigned) {
-		global $db;
-		$db->update_query('assets_users', $assigned, 'auid='.$id);
-	}
-
-	public function add_assignedasset($assigned) {
-		global $db;
-		$db->insert_query('assets_users', $assigned);
-	}
-
-	public function delete_assignedasset($id) {
-		global $db,$core;
-		if($core->usergroup['assets_canDeleteAssignement'] == 1) {
-			//echo 'deleted assignement '.$id.'<br>';
-			$db->query('delete from '.Tprefix.'assets_users where auid='.$id);
-		}
-	}
-
-	public function getAllAssets() {
-		global $db, $core;
-		$result = array();
-		$affiliates = array();
-		foreach(getAffiliateList() as $key => $value) {
-			$affiliates[] = $key;
-		}
-		$query = 'SELECT asid,title FROM '.Tprefix.'assets WHERE affid IN ('.implode(',', $affiliates).')';
-		$query = $db->query($query);
-		if($db->num_rows($query) > 0) {
-			while($row = $db->fetch_assoc($query)) {
-				$result[$row['asid']] = $row['title'];
-			}
-		}
-		return $result;
+		return $my_asid;
 	}
 
 	public function get_asset_data($from = null, $to = null, $asset_id = null) {
 		global $db;
 		if(!isset($asset_id)) {
-			$asset_id = $this->my_asid;
+			$asset_id = $my_asid;
 		}
 		$query = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate FROM '.Tprefix.'assets_locations WHERE asid='.$asset_id;
 		if(isset($from)) {
@@ -226,24 +140,13 @@ class Asset {
 
 	public function get_assets_data($asset_ids, $from = null, $to = null) {
 		global $db;
-		if(is_array($asset_ids)) {
-			$query = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate,displayName FROM '.Tprefix.'assets_locations WHERE asid IN ('.implode(',', $asset_ids).')';
-		}
-		else {
-			return null;
-		}
-
-
+		$query = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate FROM '.Tprefix.'assets_locations WHERE asid IN ('.implode(',', $asset_ids).')';
 		if(isset($from)) {
 			$query .= ' AND timeLine>'.$from;
 		}
 		if(isset($to)) {
 			$query .= ' AND timeLine<'.$to;
 		}
-		if($limit != 'All') {
-			$query .= ' LIMIT 0,'.$limit;
-		}
-
 		if(isset($cache[$query])) {
 			return $cache[$query];
 		}
@@ -259,43 +162,15 @@ class Asset {
 		return $loc;
 	}
 
-	public function get_data($limit = 'All') {
+	public function get_data_for_users($users_ids, $from, $to) {
 		global $db;
-		if($count == 'All') {
-			$query = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate,displayName FROM '.Tprefix.'assets_locations WHERE asid='.$this->my_asid.' ORDER BY timeLine DESC';
-		}
-		else {
-			$query = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate,displayName FROM '.Tprefix.'assets_locations WHERE asid='.$this->my_asid.' ORDER BY timeLine DESC Limit 0,'.$count;
-		}
-
-		if(isset($cache[$query])) {
-			return $cache[$query];
-		}
-		else {
-			$queryobj = $db->query($query);
-			if($db->num_rows($queryobj) > 0) {
-				while($row = $db->fetch_assoc($queryobj)) {
-					$loc[$row['asid']][$row['alid']] = $row;
-				}
-				$cache[$query] = $loc;
-			}
-		}
-		return $loc;
-	}
-
-	public function get_data_for_users($users_ids, $from = 0, $to = TIME_NOW, $limit = 'All') {
-		global $db;
-		$tgt = array();
-		$query = 'SELECT * FROM '.Tprefix.'assets_users WHERE uid IN ('.implode($users_ids, ',').')';
+		$query = 'SELECT * FROM '.Tprefix.'assets_users WHERE uid IN ('.implode($asset_id, ',').')';
 		if(isset($from) AND isset($to)) {
 			$query .= ' AND (toDate>'.$from.' AND fromDate<'.$to.')';
 		}
-		if($limit != 'All') {
-			$query .= ' LIMIT 0,'.$limit;
-		}
-		$query2 = $db->query($query);
-		if($db->num_rows($query2) > 0) {
-			while($row = $db->fetch_assoc($query2)) {
+		$query = $db->query($query);
+		if($db->num_rows($query) > 0) {
+			while($row = $db->fetch_assoc($query)) {
 				if($row['toDate'] > $to) {
 					$tmpTo = $row['toDate'];
 				}
@@ -312,15 +187,15 @@ class Asset {
 			}
 		}
 		foreach($tgt as $asid => $ranges) {
-			$subquery = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate FROM '.Tprefix.'assets_locations WHERE asid = '.$asid.' AND (';
+			$subquery = 'SELECT alid,asid,X(location) as latitude, Y(location) as longitude,timeLine,deviceId,speed,direction,antenna,fuel,vehiclestate,otherstate FROM '.Tprefix.'assets_locations WHERE asid IN ('.implode($asset_id, ',').') AND (';
 			$postquery = '';
 			foreach($ranges as $key => $range) {
 				$postquery.='(timeLine>'.$range["from"].' AND timeLine<'.$range["to"].') OR';
 			}
 			$subquery.= substr($postquery, 0, strlen($postquery) - 3).')';
-			$subquery2 = $db->query($subquery);
-			if($db->num_rows($subquery2) > 0) {
-				while($row = $db->fetch_assoc($subquery2)) {
+			$subquery = $db->query($subquery);
+			if($db->num_rows($subquery) > 0) {
+				while($row = $db->fetch_assoc($subquery)) {
 					$loc[$row['asid']][$row['alid']] = $row;
 				}
 			}
@@ -328,40 +203,28 @@ class Asset {
 		return $loc;
 	}
 
-	public function update_location($id, $data) {
+	public function get_map($data) { // ($uids,$asids,$from,$to) {
 		global $db;
-		$data['location'] = $data['latitude'].' '.$data['longitude'];
-		unset($data['latitude']);
-		unset($data['longitude']);
-		unset($data['alid']);
 
-		$options['geoLocation'] = array('location');
-
-		$db->update_query('assets_locations', $data, 'alid='.$id, $options);
-	}
-
-		$options['geoLocation'] = array('location');
-		$db->insert_query('assets_locations', $data, $options);
-	public function get_map($data, $options) { // ($uids,$asids,$from,$to) {
-		global $db;
-		if(isset($data)) {
-			foreach($data as $key => $trackedasset) {
-				foreach($trackedasset as $key2 => $value) {
-					$markers[] = array('title' => $value['latitude'].'|'.$value['longitude'].' -> asset('.$key.')', 'otherinfo' => 'some other info', 'geoLocation' => (number_format($value['latitude'], 6).','.number_format($value['longitude'], 6)));
-				}
+		foreach($data as $key => $trackedasset) {
+			foreach($trackedasset as $key2 => $value) {
+				$markers[] = array('title' => $value['latitude'].'|'.$value['longitude'].' ->'.$key.':'.Maps::get_streetname($value['latitude'], $value['longitude']), 'otherinfo' => 'some other info', 'geoLocation' => (number_format($value['latitude'], 6).','.number_format($value['longitude'], 6)));
 			}
-			$options['overlaytype'] = 'parsePolylines';
+		}
+
 		$options = array('overlaytype' => 'parsePolylines');
 		$map = new Maps($markers, array('infowindow' => 1, 'mapcenter' => '32.887078, 34.195312'), $options);
-			$map_view = $map->get_map(500, 400);
-			return $map_view;
-		}
-		else {
+		$map_view = $map->get_map(400, 300);
+		return $map_view.'<hr><pre >'.$map->get_streetname($lat, $long).'</pre>';
+	}
+
 	private function read($id, $simple = false) {
+		global $db;
 
 		if(empty($id)) {
 			return false;
 		}
+
 		$query_select = '*';
 		if($simple == true) {
 			$query_select = 'asid, title, type';
@@ -371,6 +234,7 @@ class Asset {
 	}
 
 	public function get_allassets() {
+		global $db;
 		$allassets = $db->query("SELECT asid,title,description FROM ".Tprefix."assets");
 		while($assets = $db->fetch_assoc($allassets)) {
 			$asset[$assets['asid']] = $assets;
