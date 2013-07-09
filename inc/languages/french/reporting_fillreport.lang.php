e5080ea66e12aad654bd1149525dc2b32220df77
tony.assaad <tony.assaad@orkila.com>
2013-07-09 17:56:33 +0300

    adding wrongforecastgoback index to the french version in the fill report

--- inc/languages/french/reporting_fillreport.lang.php
+++ inc/languages/french/reporting_fillreport.lang.php
@@ -5,39 +5,40 @@ $lang['selectareport'] = "Selectionner un Rapport";
 $lang['noreportsavailable'] = "Aucun rapport n'est disponible";
 $lang['keycustomer'] = "Client Principal";
 $lang['excludekeycustomers'] = "Exclure Clients Principaux";
-$lang['excludeproductsactivity'] = "Exclure produits d'activité";
+$lang['excludeproductsactivity'] = "Exclure produits d'activitï¿½";
 
-$lang['savedsuccessfully'] = "Sauvegarde Effectuée";
-$lang['saveerror'] = "Une erreur s'est produite lors de l'enregistrement des données";
-$lang['fillatleastoneproductrow'] = "Remplissez au moins une rangée de produit";
+$lang['savedsuccessfully'] = "Sauvegarde Effectuï¿½e";
+$lang['saveerror'] = "Une erreur s'est produite lors de l'enregistrement des donnï¿½es";
+$lang['fillatleastoneproductrow'] = "Remplissez au moins une rangï¿½e de produit";
 $lang['fillatleastonecustomerrow'] = "Remplissez au moins un client";
-$lang['fillonemktreportsection'] = "Remplissez au moins la section du rapport de marché";
+$lang['fillonemktreportsection'] = "Remplissez au moins la section du rapport de marchï¿½";
 
-$lang['generic'] = "Générique";
+$lang['generic'] = "Gï¿½nï¿½rique";
 $lang['code'] = "Code";
 $lang['name'] = "Nom";
 $lang['cancel'] = "Annuler";
 
 $lang['companyname'] = "Nom de l'entreprise";
-$lang['representativename'] = "Nom du représentant";
+$lang['representativename'] = "Nom du reprï¿½sentant";
 $lang['country'] = "Pays";
 
 $lang['addnewproduct'] = "Ajouter un Nouveau Produit";
-$lang['productadded'] = "Produit {1} a été ajouté";
+$lang['productadded'] = "Produit {1} a ï¿½tï¿½ ajoutï¿½";
 $lang['erroraddingproduct'] = "Une erreur s'est produite lors de l'ajout du produit";
-$lang['productalreadyexists'] = "Un produit portant le même nom existe déjà";
+$lang['productalreadyexists'] = "Un produit portant le mï¿½me nom existe dï¿½jï¿½";
 $lang['noproductsavailable'] = "Aucun produit n'est encore disponible";
 
 $lang['addnewcustomer'] = "Ajouter un nouveau client";
-$lang['specifycompanyname'] = "Veuillez indiquer un nom de société";
+$lang['specifycompanyname'] = "Veuillez indiquer un nom de sociï¿½tï¿½";
 $lang['specifyanaffiliate'] = "Veuillez indiquer un nom de filiale";
-$lang['entitycreated'] = "Entité crée avec succès";
-$lang['errorcreatingentity'] = "Erreur de création de l'entité";
-$lang['joinedsuccessfully'] = "Inscription réussie";
-$lang['entityalreadyexists'] = "Une entité ayant le même nom existe déjà";
-$lang['invalidentityemail'] = "Un e-mail d'entité non valide a été fourni";
-$lang['specifyrepemail'] = "Précisez l'email du représentant ";
-$lang['representativeexists']=  "Un représentant du même nom existe déjà";
-$lang['representativecreated'] = "Le représentant a été créé avec succès";
-$lang['errorcreatingreprentative'] = "Erreur de création du représentant"; 
+$lang['entitycreated'] = "Entitï¿½ crï¿½e avec succï¿½s";
+$lang['errorcreatingentity'] = "Erreur de crï¿½ation de l'entitï¿½";
+$lang['joinedsuccessfully'] = "Inscription rï¿½ussie";
+$lang['entityalreadyexists'] = "Une entitï¿½ ayant le mï¿½me nom existe dï¿½jï¿½";
+$lang['invalidentityemail'] = "Un e-mail d'entitï¿½ non valide a ï¿½tï¿½ fourni";
+$lang['specifyrepemail'] = "Prï¿½cisez l'email du reprï¿½sentant ";
+$lang['representativeexists']=  "Un reprï¿½sentant du mï¿½me nom existe dï¿½jï¿½";
+$lang['representativecreated'] = "Le reprï¿½sentant a ï¿½tï¿½ crï¿½ï¿½ avec succï¿½s";
+$lang['errorcreatingreprentative'] = "Erreur de crï¿½ation du reprï¿½sentant"; 
+$lang['wrongforecastgoback'] = 'les prÃ©visions de fin d\'annÃ©e sont fausses, merci de les corriger';
 ?>
