f66ab7267e891f9fbc5b527e2ef1d6e93db6f81a
zaher.reda <zaher.reda@orkila.com>
2013-07-29 17:04:48 +0300

    Applied fixes & optimisation for auto segement assign
    1. Moved the auto assign to after product has been added
    2. gpid is not optional anymore

--- manage/modules/products/add.php
+++ manage/modules/products/add.php
@@ -46,16 +46,16 @@ else
 			output_xml("<status>false</status><message>{$lang->productalreadyexists}</message>"); 
 			exit;
 		}
-		$entite = new Entities($core->input['spid']);
-		$entite->auto_assignsegment($core->input['gpid']);
-		log_action($core->input['name']);
 		unset($core->input['action'], $core->input['module']);
-		
 		//Temporary hardcode
 		$core->input['defaultCurrency'] = "USD";
 		
 		$query = $db->insert_query("products", $core->input);
 		if($query) {
+			$entity = new Entities($core->input['spid']);
+			$entity->auto_assignsegment($core->input['gpid']);
+			$log->record($core->input['name']);
+		
 			$lang->productadded = $lang->sprint($lang->productadded, htmlspecialchars($core->input['name']));
 			output_xml("<status>true</status><message>{$lang->productadded}</message>");
 		}
