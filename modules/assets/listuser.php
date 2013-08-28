adb231403ae32d759913e58e51de97a0af5fe19b
zaher.reda <zaher.reda@orkila.com>
2013-08-28 15:59:27 +0300

    Fixed issues in list users
    1. Filters list now only show values that can be seen by user
    2. Fixed issue with mulipage
    3. Optimized how disabled fields are set
    4. Fixed issue with users list in edit assignment

--- modules/assets/listuser.php
+++ modules/assets/listuser.php
@@ -14,13 +14,18 @@ if($core->usergroup['assets_canManageAssets'] == 0) {
 }
 
 if(!$core->input['action']) {
+	$sort_url = sort_url();
+	
 	$asset = new Assets();
-
+	$affiliate = new Affiliates($core->user['mainaffiliate']);
+	$affiliate_users = $affiliate->get_users(array('ismain' => 1, 'displaynameonly' => 1));
+		
 	/* Perform inline filtering - START */
 	$filters_config = array(
 			'parse' => array('filters' => array('uid', 'asid', 'fromDate', 'toDate', 'toDate'),
-					'overwriteField' => array('uid' => parse_selectlist('filters[uid]', 1, $asset->get_assignto(), '', '', '', array('blankstart' => true)),
-							'asid' => parse_selectlist('filters[asid]', 2, get_specificdata('assets', array('asid', 'title'), 'asid', 'title', 'affid in('.implode(',', $core->user['affiliates']).')'), '', '', '', array('blankstart' => true))
+					'overwriteField' => array(
+							'uid' => parse_selectlist('filters[uid]', 1, $affiliate_users, $core->input['filters']['uid'], '', '', array('blankstart' => true)),
+							'asid' => parse_selectlist('filters[asid]', 2, get_specificdata('assets', array('asid', 'title'), $core->input['filters']['asid'], 'title', 'affid IN ('.implode(',', $core->user['affiliates']).')'), '', '', '', array('blankstart' => true))
 					),
 					'fieldsSequence' => array('uid' => 1, 'asid' => 2, 'fromDate' => 3, 'toDate' => 4)
 			/* get the busieness potential and parse them in select list to pass to the filter array */
@@ -41,20 +46,24 @@ if(!$core->input['action']) {
 	if(isset($core->input['perpage']) && !empty($core->input['perpage'])) {
 		$core->settings['itemsperlist'] = $db->escape_string($core->input['perpage']);
 	}
+	
+	if(true) {/* Later to be, if has permission to view multiple affiliates */	
+		$multipage_where = 'affid = '.$core->user['mainaffiliate'];
+	} else {
+		$multipage_where = 'affid IN ('.$db->escape_string(implode(',', $core->user['affiliates'])).')';
+	}
+	
 	if(is_array($filter_where_values)) {
 		$filters_row_display = 'show';
 		$filter_where = $filters_config['process']['filterKey'].' IN ('.implode(',', $filter_where_values).')';
-		$multipage_where .= $filters_config['process']['filterKey'].' IN ('.implode(',', $filter_where_values).')';
+		$multipage_where .= ' AND '.$filters_config['process']['filterKey'].' IN ('.implode(',', $filter_where_values).')';
 	}
 
 	$filters_row = $filter->prase_filtersrows(array('tags' => 'table', 'display' => $filters_row_display));
 	/* Perform inline filtering - END */
 	
-	$multipage_where .= $db->escape_string($attributes_filter_options['prefixes'][$core->input['filterby']].$core->input['filterby']).$filter_value;
 	$assignee = $asset->get_allassignee($filter_where);
 	
-	$sort_url = sort_url();
-
 	if(is_array($assignee)) {
 		foreach($assignee as $auid => $assigneduser) {
 			$rowclass = alt_row($rowclass);
@@ -77,56 +86,66 @@ if(!$core->input['action']) {
 			eval("\$assignee_list .= \"".$template->get('assets_assignlist_row')."\";");
 		}
 		
-		$multipages = new Multipages('assets_users', $core->settings['itemsperlist'], $multipage_where);
-		$assignee_list .= '<tr><td colspan="7">'.$multipages->parse_multipages().'</td></tr>';
+		$multipages = new Multipages('assets_users au JOIN '.Tprefix.'assets a ON (a.asid=au.asid)', $core->settings['itemsperlist'], $multipage_where);
+		$assignee_list .= '<tr><td colspan="5">'.$multipages->parse_multipages().'</td></tr>';
 	}
 	else {
-		$assignee_list = '<tr><td colspan="7">'.$lang->na.'</td></tr>';
+		$assignee_list = '<tr><td colspan="5">'.$lang->na.'</td></tr>';
 	}
 
 	eval("\$assetsassignlist = \"".$template->get('assets_assignlist')."\";");
 	output_page($assetsassignlist);
 }
-elseif($core->input['action'] == 'get_deleteuser') {
-	eval("\$deleteassignee = \"".$template->get('popup_assets_listuserdelete')."\";");
-	echo $deleteassignee;
-}
-elseif($core->input['action'] == 'perform_delete') {
-	$auid = $db->escape_string($core->input['todelete']);
-	
-	$asset = new Assets();
-	$assignee = $asset->get_assigneduser($auid);
-	if(TIME_NOW > ($assignee['assignedon'] + ($core->settings['assets_preventeditasgnafter']))) {
-		output_xml("<status>false</status><message>{$lang->notpossibledelete}</message>");
-		exit;
+else {
+	if($core->input['action'] == 'get_deleteuser') {
+		eval("\$deleteassignee = \"".$template->get('popup_assets_listuserdelete')."\";");
+		echo $deleteassignee;
 	}
+	elseif($core->input['action'] == 'perform_delete') {
+		$auid = $db->escape_string($core->input['todelete']);
+
+		$asset = new Assets();
+		$assignee = $asset->get_assigneduser($auid);
+		if(TIME_NOW > ($assignee['assignedOn'] + ($core->settings['assets_preventeditasgnafter']))) {
+			output_xml("<status>false</status><message>{$lang->notpossibledelete}</message>");
+			exit;
+		}
+
+		$asset->delete_userassets($auid);
+		switch($asset->get_errorcode()) {
+			case 3:
+				output_xml("<status>true</status><message>{$lang->successfullydeleted}</message>");
+				break;
+			case 604:
+				output_xml("<status>false</status><message>{$lang->errorsaving}</message>");
+				break;
+		}
+	}
+	elseif($core->input['action'] == 'get_edituser') {
+		$asset = new Assets();
+		$auid = $db->escape_string($core->input['id']);
+		$assignee = $asset->get_assigneduser($auid);
+		$assetslist = $asset->get_affiliateassets(array('titleonly' => 1, 'mainaffidonly' => 1));
+
+		if(TIME_NOW > ($assignee['assignedon'] + ($core->settings['assets_preventeditasgnafter']))) {
+			$disabled_fields['asid'] = $disabled_fields['uid'] = array('disabled' => 'disabled');
+			$fields_todisable = array('conditionOnHandover', 'fromDate', 'toDate', 'fromTime', 'toTime');
+			foreach($fields_todisable as $item) {
+				$disabled_fields[$item] = ' disabled="disabled"';
+			}
+		}
+		if(TIME_NOW > ($assignee['assignedon'] + ($core->settings['assets_preventconditionupdtafter']))) {
+			$disabled_fields['conditionOnReturn'] = ' disabled="disabled"';	
+		}
+		$assets_selectlist = parse_selectlist('assignee[asid]', 1, $assetslist, $assignee['asid'], '', '', $disabled_fields['asid']);
 		
-	$asset->delete_userassets($auid);
-	switch($asset->get_errorcode()) {
-		case 3:
-			output_xml("<status>true</status><message>{$lang->successfullydeleted}</message>");
-			break;
-	}
-}
-elseif($core->input['action'] == 'get_edituser') {
-	$asset = new Assets();
-	$auid = $db->escape_string($core->input['id']);
-	$assignee = $asset->get_assigneduser($auid);
-	$assetslist = $asset->get_affiliateassets('titleonly');
+		$affiliate = new Affiliates($core->user['mainaffiliate']);
+		$affiliate_users = $affiliate->get_users(array('ismain' => 1, 'displaynameonly' => 1));
+		$employees_selectlist = parse_selectlist('assignee[uid]', 1, $affiliate_users, $assignee['uid'], '', '', $disabled_fields['asid']);
+		$actiontype = 'edit';
 
-	if(TIME_NOW > ($assignee['assignedon'] + ($core->settings['assets_preventeditasgnafter']))) {
-		$disable_list = array('disabled' => 'disabled');
-		$disable_text = ' disabled="disabled"';
+		eval("\$editassignee = \"".$template->get('popup_assets_listuseredit')."\";");
+		echo $editassignee;
 	}
-	if(TIME_NOW > ($assignee['assignedon'] + ($core->settings['assets_preventconditionupdtafter']))) {
-		$disable_cor = ' disabled="disabled"';
-	}
-	$assets_list = parse_selectlist('assignee[asid]', 1, $assetslist, $assignee['asid'], '', '', $disable_list);
-	$assigners = $asset->get_assignto();
-	$employees_list = parse_selectlist('assignee[uid]', 1, $assigners, $assignee['uid'], '', '', $disable_list);
-	$actiontype = 'edit';
-
-	eval("\$editassignee = \"".$template->get('popup_assets_listuseredit')."\";");
-	echo $editassignee;
 }
 ?>
