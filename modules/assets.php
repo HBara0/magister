8ebcc2bf44a6a64888ec1be3cb5c56c275d485c2
zaher.reda <zaher.reda@orkila.com>
2013-08-30 13:25:22 +0300

    Fixed page names in assets module config file

--- modules/assets.php
+++ modules/assets.php
@@ -3,8 +3,8 @@ $module['name'] = 'assets';
 $module['title'] = $lang->assetsmodule;
 $module['homepage'] = 'listassets';
 $module['globalpermission'] = 'canUseAssets';
-$module['menu'] = array('file' 		  => array('assignassets','listuser','manageassets','listassets'),
-						'title'		 => array('assignassetstitle','listusertitle','assetspage','listasset'),
+$module['menu'] = array('file' 		  => array('assignassets','listusers','addassets','listassets'),
+						'title'		 => array('assignassetstitle','listusertitle','addassets','listasset'),
 						'permission'	=> array('assets_canManageAssets','assets_canManageAssets','assets_canManageAssets', 'assets_canManageAssets')
 						);
 
