9bf16b49c2ebc5a3844e3f8ab1a0542310a10694
zaher.reda <zaher.reda@orkila.com>
2013-09-01 11:14:25 +0300

    Replaced query by read_usergroups function of Users Class in Sessions Class

--- inc/Sessions_class.php
+++ inc/Sessions_class.php
@@ -27,10 +27,7 @@ class Sessions {
 			if($user) {
 				$core->user = $user->get();
 				unset($core->user['password'], $core->user['salt']);
-				
-				$query2 = $db->query("SELECT * FROM ".Tprefix."usergroups WHERE gid='".$core->user['gid']."'");
-				$core->usergroup = $db->fetch_assoc($query2);
-
+				$user->read_usergroups();
 				/* $auditing = $db->query("SELECT eid FROM ".Tprefix."suppliersaudits WHERE uid='".$this->uid."'");
 				  if($db->num_rows($auditing) > 0) {
 				  while($auditfor = $db->fetch_assoc($auditing)) {
