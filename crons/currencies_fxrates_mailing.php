fcbb1d9bd0760bd8efe64bd154931c6bd8a319c0
tony.assaad <tony.assaad@orkila.com>
2013-09-06 13:59:10 +0300

    modifications done on currencies_fxrates cron :
    Show Reverse Rates with the  USD- vs original affiliate currency,

--- crons/currencies_fxrates_mailing.php
+++ crons/currencies_fxrates_mailing.php
@@ -23,14 +23,14 @@ if($_REQUEST['authkey'] == 'asfasdkj%2j!h4k23jh4k2_3h4k23jh') {
 	/* Get Fin managers - START */
 	$query = $db->query('SELECT finManager FROM '.Tprefix.'affiliates WHERE finManager IS NOT NULL');
 
-	$users_list = array(66=>66);
+	$users_list = array(66 => 66);
 	if($db->num_rows($query) > 0) {
 		while($finmanager = $db->fetch_assoc($query)) {
 			$users_list[$finmanager['finManager']] = $finmanager['finManager'];
 		}
 	}
 	/* Get Fin managers - END */
-	
+
 	$query = $db->query('SELECT u.uid, displayName, email, name, a.affid
 				FROM '.Tprefix.'affiliatedemployees a
 				INNER JOIN '.Tprefix.'users u ON (a.uid = u.uid)
@@ -52,7 +52,7 @@ if($_REQUEST['authkey'] == 'asfasdkj%2j!h4k23jh4k2_3h4k23jh') {
 				FROM '.Tprefix.'countries c 
 				INNER JOIN '.Tprefix.'currencies cur ON (c.mainCurrency = cur.numCode)
 				WHERE affid<>0');
-	
+
 	while($country_currency = $db->fetch_assoc($affiliatecurrenciesquery)) {
 		$affiliates_currencies[$country_currency[affid]][$country_currency['alphaCode']]['name'] = $country_currency['name'];
 		if(!isset($fxrates[$country_currency['alphaCode']]['latest'])) {
@@ -62,8 +62,9 @@ if($_REQUEST['authkey'] == 'asfasdkj%2j!h4k23jh4k2_3h4k23jh') {
 			$fxrates[$country_currency['alphaCode']]['average'] = $currency_obj->get_average_fxrate($country_currency['alphaCode'], array('from' => $from, 'to' => $to));
 		}
 	}
-	
+
 	foreach($finmanagers as $uid => $user) {
+		$user['email'] = 'tony.assaad@orkila.com';
 		$email_data['to'] = $user['email'];
 		$email_data['message'] = '<pre>Dear '.$user['name'].",\n\n";
 		$email_data['message'] .= "Please find below the average USD exchange rates for the past month\n\n";
@@ -78,10 +79,12 @@ if($_REQUEST['authkey'] == 'asfasdkj%2j!h4k23jh4k2_3h4k23jh') {
 				$rates['average'] = $rates['latest'];
 			}
 			$email_data['message'] .= '['.$alphacode.'] Avg: '.formatit($rates['average']).' Last: '.formatit($rates['latest'])."\n";
+			if(!empty($rates['average']) && !empty($rates['latest'])) {
+				$email_data['message'].= '[USD]:['.$alphacode.'] '.formatit(1 / $rates['average']).' Last: '.formatit(1 / $rates['latest'])."\n";
+			}
 		}
-		
-		//$email_data['message'].="\nBest Regards,\nSignature</pre>";
-		
+
+		$email_data['message'].="\nBest Regards,\nSignature</pre>";
 		$mail = new Mailer($email_data, 'php');
 		if($mail->get_status() == true) {
 			$log->record($user['name'], 'success');
@@ -94,7 +97,6 @@ if($_REQUEST['authkey'] == 'asfasdkj%2j!h4k23jh4k2_3h4k23jh') {
 else {
 	die('Unauthorized Access');
 }
-
 function formatit($number) {
 	if(isset($number)) {
 		return str_pad(round(number_format($number, 6, '.', ''), 6), 11, ' ', STR_PAD_LEFT);
@@ -103,4 +105,5 @@ function formatit($number) {
 		return str_pad('-', 11, ' ', STR_PAD_LEFT);
 	}
 }
+
 ?>
