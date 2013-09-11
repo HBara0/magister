804a4fe78f050d9f83db5b026e79c760321061dd
tony.assaad <tony.assaad@orkila.com>
2013-09-11 10:54:53 +0300

    code optimization done :
    1-fixing bug to send all events of the same affiliate in the same email.
    1-only  Public event will be selected and send

--- crons/calendar_suppliervisitus.php
+++ crons/calendar_suppliervisitus.php
@@ -16,11 +16,10 @@ if($_REQUEST['authkey'] == 'asfasdkjj!h4k23jh4k2_3h4k23jh') {
 	$affiliates_query = $db->query("SELECT aff.*, ce.* 
 								FROM ".Tprefix."calendar_events ce 
 								JOIN ".Tprefix."affiliates aff ON (aff.affid=ce.affid)
-								WHERE ce.fromDate BETWEEN ".strtotime('tomorrow')." AND ".strtotime('+2 days -1 second', strtotime('today')));
+								WHERE isPublic=1 AND ce.fromDate BETWEEN ".strtotime('tomorrow')." AND ".strtotime('+2 days -1 second', strtotime('today')));
 
 	if($db->num_rows($affiliates_query) > 0) {
 		while($affiliates = $db->fetch_assoc($affiliates_query)) {
-//			$aff_obj = new Affiliates($affiliates['affid'], false);
 			$affiliate_events[$affiliates['affid']][$affiliates['ceid']] = $affiliates;
 		}
 	}
@@ -31,35 +30,24 @@ if($_REQUEST['authkey'] == 'asfasdkjj!h4k23jh4k2_3h4k23jh') {
 			$aff_obj = new Affiliates($affid, false);
 			$affiliate = $aff_obj->get();
 
-			foreach($events as $data) {
-				$event['title'] = $data['title'];
-				$event['fromDate'] = $data['fromDate'];
-				$event['toDate'] = $data['toDate'];
-				$event['place'] = $data['place'];
-				$event['description'] = $data['description'];
-			}
-			$body_message .= '<ul><li>'.$lang->sprint($lang->suppliervisit_reminder_message, $affiliate['name'], $event['title']).'</li></ul>';
-
-			if(empty($body_message)) {
-				continue;
-			}
 			$email_data = array(
 					'to' => $affiliate['mailingList'],
 					'from_email' => $core->settings['adminemail'],
 					'from' => 'OCOS Mailer',
 					'subject' => $lang->suppliervisit_subject,
-					//'message' => $body_message
 			);
-			$email_data['message'] = '<strong>'.$event['title'].'</strong> (';
 
-			$email_data['message'] .= date($core->settings['dateformat'], $event['fromDate']);
-			if($event['toDate'] != $event['fromDate']) {
-				$email_data['message'] .= ' - '.date($core->settings['dateformat'], $event['toDate']);
+			foreach($events as $event) {
+				$email_data['message'] .= '<strong>'.$event['title'].'</strong> (';
+
+				$email_data['message'] .= date($core->settings['dateformat'], $event['fromDate']);
+				if($event['toDate'] != $event['fromDate']) {
+					$email_data['message'] .= ' - '.date($core->settings['dateformat'], $event['toDate']);
+				}
+				$email_data['message'] .= ')<br />';
+				$email_data['message'] .= str_replace("\n", '<br />', $event['description']).'<br />';
+				$email_data['message'] .= $event['place'].'<br /><br />';
 			}
-			$email_data['message'] .= ')<br />';
-			$email_data['message'] .= $event['place'].'<br />';
-			$email_data['message'] .= str_replace("\n", '<br />', $event['description']);
-
 			$mail = new Mailer($email_data, 'php');
 			if($mail->get_status() === true) {
 				$log->record('suppliervisitreminder', array('to' => $affiliate['mailingList']));
