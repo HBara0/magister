e660cc702aac189f022468f742ce18c9adbf5810
tony.assaad <tony.assaad@orkila.com>
2013-10-03 15:41:19 +0300

    adding new menu to the budgeting file

--- modules/budgeting.php
+++ modules/budgeting.php
@@ -3,9 +3,9 @@ $module['name'] = 'budgeting';
 $module['title'] = $lang->budget;
 $module['homepage'] = 'create';
 $module['globalpermission'] = 'canUseBudgeting';
-$module['menu'] = array('file' 		  => array('create','generatebudget'),
-						'title'		 => array('create','generate'),
-						'permission'	=> array('budgeting_canFillBudget','budgeting_canFillBudget')
+$module['menu'] = array('file' 		  => array('create','generatebudget','importbudget'),
+						'title'		 => array('create','generate','importbudget'),
+						'permission'	=> array('budgeting_canFillBudget','budgeting_canFillBudget','budgeting_canFillBudget')
 						);
 
 ?>
