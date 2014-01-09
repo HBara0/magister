8ee5cdced730f9740e8a1e5571e5e683d500b35f
tony.assaad <tony.assaad@orkila.com>
2014-01-09 11:41:41 +0200

    hot fixes  handling users object when when viewing all leaves in list leaves

--- modules/attendance/listleaves.php
+++ modules/attendance/listleaves.php
@@ -158,7 +158,9 @@ if(!$core->input['action']) {
 			}
 			$edit_link = $revoke_link = '';
 			$user_obj = new Users($leave['uid']);
-			$reports_to = $user_obj->get_reportsto()->get()['uid'];
+			if(is_object($user_obj)) {
+				$reports_to = $user_obj->get_reportsto()->get()['uid'];
+			}
 			if($core->usergroup['attenance_canApproveAllLeaves'] == 1 || (($core->usergroup['hr_canHrAllAffiliates'] == 1 || $reports_to == $core->user['uid'] || $leave['uid'] == $core->user['uid']) && TIME_NOW < ($leave['toDate'] + (60 * 60 * 24 * $core->settings['attendance_caneditleaveafter'])) || (TIME_NOW > $leave['toDate'] && $status['approved'] != array_sum($status)))) {
 				$edit_link = "<a href='index.php?module=attendance/editleave&amp;lid={$leave[lid]}'><img src='{$core->settings[rootdir]}/images/icons/edit.gif' border='0' alt='{$lang->modifyleave}' /></a>";
 				$revoke_link = "<a href='#{$leave[lid]}' id='revokeleave_{$leave[lid]}_attendance/listleaves_icon'><img src='{$core->settings[rootdir]}/images/invalid.gif' border='0' alt='{$lang->revokeleave}' /></a>";
