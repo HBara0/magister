3b698c8754e22d5800f22a67682cd8edb680fb8c
zaher.reda <zaher.reda@orkila.com>
2014-02-11 09:48:39 +0200

    Major updates to the stock aging report with new summary boxes

--- integration/ob_report_stockaging.php
+++ integration/ob_report_stockaging.php
@@ -13,6 +13,7 @@ require '../inc/init.php';
 define('AUTHCODE', 'X1iIMm7pG06ip6o95HEa');
 if($core->input['authCode'] == AUTHCODE) {
 	$currency_obj = new Currencies('USD');
+	$date_info = getdate_custom(TIME_NOW);
 
 	$db_info = array('database' => 'openbrav_production', 'username' => 'openbrav_appuser', 'password' => '8w8;MFRy4g^3', 'engine' => 'postgre');
 
@@ -20,6 +21,13 @@ if($core->input['authCode'] == AUTHCODE) {
 			'C08F137534222BD001345BAA60661B97' => 19, //Orkila Tunisia
 			'0B366EFAE0524FDAA97A1322A57373BB' => 22, //Orkila East Africa
 			'DA0CE0FED12C4424AA9B51D492AE96D2' => 11, //Orkila Nigeria
+			'F2347759780B43B1A743BEE40BA213AD' => 23, //Orkila Ghana
+	);
+
+	$affiliates_addrecpt = array(
+			19 => array(244),
+			22 => array(248, 246, 287, 270),
+			23 => array(285, 322, 321)
 	);
 
 	$integration = new IntegrationOB($db_info, 'C08F137534222BD001345B7B2E8F182D', $affiliates_index, 3, array('from' => 'last year'));
@@ -30,39 +38,50 @@ if($core->input['authCode'] == AUTHCODE) {
 		exit;
 	}
 
-	$report_options = array('roundto' => 2);
+	$report_options = array('roundto' => 0);
 
 	$output_fields = array(
 //			'manager' => 'Business Manager',
 			'product' => array('source' => 'product', 'attribute' => 'name', 'title' => 'Product'),
-			'packaging' => array('source' => array('transaction', 'attributes'), 'attribute' => 'packaging', 'title' => 'Packaging'),
-			'quantity' => 'Quantity',
+			'supplier' => array('source' => 'supplier', 'attribute' => 'value', 'title' => 'Supplier'),
+			'warehouse' => array('source' => 'warehouse', 'attribute' => 'value', 'title' => 'Warehouse'),
+			'category' => array('source' => array('product', 'category'), 'attribute' => 'value', 'title' => 'Segment'),
+			//'packaging' => array('source' => array('transaction', 'attributes'), 'attribute' => 'packaging', 'title' => 'Packaging'),
+			//'quantity' => 'Quantity',
+			'quantity' => array('source' => null, 'title' => 'Stock Qty', 'numformat' => true),
 			'uom' => array('source' => array('product', 'uom'), 'attribute' => 'uomsymbol', 'title' => 'UoM'),
-			'cost' => 'Cost',
-			'range1cost' => array('source' => array('entries', 'cost'), 'attribute' => 1, 'title' => '0-29<br />Amt', 'numformat' => true, 'style' => 'background-color: #ABD25E;'),
-			'range1qty' => array('source' => array('entries', 'qty'), 'attribute' => 1, 'title' => '0-29<br />Qty', 'numformat' => true, 'style' => 'background-color: #ABD25E;'),
-			'range2cost' => array('source' => array('entries', 'cost'), 'attribute' => 2, 'title' => '30-59<br />Amt', 'numformat' => true, 'style' => 'background-color: #B8E1F2;'),
-			'range2qty' => array('source' => array('entries', 'qty'), 'attribute' => 2, 'title' => '30-59<br />Qty', 'numformat' => true, 'style' => 'background-color: #B8E1F2;'),
-			'range3cost' => array('source' => array('entries', 'cost'), 'attribute' => 3, 'title' => '60-89<br />Amt', 'numformat' => true, 'style' => 'background-color: #F2EB80;'),
-			'range3qty' => array('source' => array('entries', 'qty'), 'attribute' => 3, 'title' => '60-89<br />Qty', 'numformat' => true, 'style' => 'background-color: #F2EB80;'),
-			'range4cost' => array('source' => array('entries', 'cost'), 'attribute' => 4, 'title' => '90-119<br />Amt', 'numformat' => true, 'style' => 'background-color: #F8C830;'),
-			'range4qty' => array('source' => array('entries', 'qty'), 'attribute' => 4, 'title' => '90-119<br />Qty', 'numformat' => true, 'style' => 'background-color: #F8C830;'),
-			'range5cost' => array('source' => array('entries', 'cost'), 'attribute' => 5, 'title' => '> 120<br />Amt', 'numformat' => true, 'style' => 'background-color: #F1594A;'),
-			'range5qty' => array('source' => array('entries', 'qty'), 'attribute' => 5, 'title' => '> 120<br />Qty', 'numformat' => true, 'style' => 'background-color: #F1594A;')
+			//'cost' => 'Cost',
+			'cost' => array('source' => null, 'title' => 'Cost', 'numformat' => true),
+			'range1cost' => array('source' => array('entries', 'cost'), 'attribute' => 1, 'title' => '0-29<br />Amt', 'numformat' => true, 'styles' => 'background-color: #ABD25E;'),
+			'range1qty' => array('source' => array('entries', 'qty'), 'attribute' => 1, 'title' => '0-29<br />Qty', 'numformat' => true, 'styles' => 'background-color: #ABD25E;'),
+			'range2cost' => array('source' => array('entries', 'cost'), 'attribute' => 2, 'title' => '30-59<br />Amt', 'numformat' => true, 'styles' => 'background-color: #B8E1F2;'),
+			'range2qty' => array('source' => array('entries', 'qty'), 'attribute' => 2, 'title' => '30-59<br />Qty', 'numformat' => true, 'styles' => 'background-color: #B8E1F2;'),
+			'range3cost' => array('source' => array('entries', 'cost'), 'attribute' => 3, 'title' => '60-89<br />Amt', 'numformat' => true, 'styles' => 'background-color: #F2EB80;'),
+			'range3qty' => array('source' => array('entries', 'qty'), 'attribute' => 3, 'title' => '60-89<br />Qty', 'numformat' => true, 'styles' => 'background-color: #F2EB80;'),
+			'range4cost' => array('source' => array('entries', 'cost'), 'attribute' => 4, 'title' => '90-119<br />Amt', 'numformat' => true, 'styles' => 'background-color: #F8C830;'),
+			'range4qty' => array('source' => array('entries', 'qty'), 'attribute' => 4, 'title' => '90-119<br />Qty', 'numformat' => true, 'styles' => 'background-color: #F8C830;'),
+			'range5cost' => array('source' => array('entries', 'cost'), 'attribute' => 5, 'title' => '> 120<br />Amt', 'numformat' => true, 'styles' => 'background-color: #F1594A;'),
+			'range5qty' => array('source' => array('entries', 'qty'), 'attribute' => 5, 'title' => '> 120<br />Qty', 'numformat' => true, 'styles' => 'background-color: #F1594A;')
 	);
 
-	$totals = array('quantitysold', 'quantity', 'cost', 'costusd');
+	$summary_categories = array('category' => 'm_product_category_id', 'warehouse' => 'm_warehouse_id', 'supplier' => 'c_bpartner_id');
+	$summary_reqinfo = array('quantity', 'cost', 'range1cost', 'range1qty', 'range2cost', 'range2qty', 'range3cost', 'range3qty', 'range4cost', 'range4qty', 'range5cost', 'range5qty');
+	$summary_order_attr = 'cost';
+	$maintable_hiddencols = array('supplier', 'warehouse', 'category');
+
+	$total_types = array('quantity', 'cost', 'range1cost', 'range1qty', 'range2cost', 'range2qty', 'range3cost', 'range3qty', 'range4cost', 'range4qty', 'range5cost', 'range5qty');
 	foreach($affiliates_index as $orgid => $affid) {
 		$output = '';
 		$inputs = array();
-		$affiliateobj = new Affiliates($affid);
+		$totals = $summaries = array();
+		$affiliateobj = new Affiliates($affid, false);
 		$affiliate = $affiliateobj->get();
 		$affiliate['currency'] = $affiliateobj->get_country()->get_maincurrency()->get()['alphaCode'];
-		$output = '<h3>'.$affiliate['name'].' ( '.$affiliate['currency'].')</h3>';
+		$output = '<h3>'.$affiliate['name'].' - Week '.$date_info['week'].' ( '.$affiliate['currency'].')</h3>';
 		$rawinputs = $integration->get_fifoinputs(array($orgid), array('hasqty' => true));
 
-		foreach($rawinputs as $input) {
-			$inputs[$input['product']['m_product_id']]['product'] = $input['product'];
+		foreach($rawinputs as $key => $input) {
+			//$inputs[$input['product']['m_product_id']]['product'] = $input['product'];
 
 			if($input['stack']['daysinstock'] < 29) {
 				$range = 1;
@@ -79,26 +98,31 @@ if($core->input['authCode'] == AUTHCODE) {
 			else {
 				$range = 5;
 			}
-			$inputs[$input['product']['m_product_id']]['entries']['qty'][$range] += $input['stack']['remaining_qty'];
-			$inputs[$input['product']['m_product_id']]['entries']['cost'][$range] += $input['stack']['remaining_cost'];
+			$rawinputs[$key]['entries']['qty'][$range] += $input['stack']['remaining_qty'];
+			$rawinputs[$key]['entries']['cost'][$range] += $input['stack']['remaining_cost'];
 		}
 
-		$output .= '<table width="100%" border="1" padding="2px">';
+		$output .= '<table width="100%" cellspacing="0" cellpadding="5" style="border: 1px solid #CCC; font-size: 10px;" border="0">';
 		$output .= '<tr>';
 		foreach($output_fields as $field => $configs) {
+			if(in_array($field, $maintable_hiddencols)) {
+				continue;
+			}
 			if(is_array($configs)) {
-				$output .= '<th>'.$configs['title'].'</th>';
+				$output .= '<th style="background: #91b64f;">'.$configs['title'].'</th>';
 			}
 			else {
-				$output .= '<th>'.$configs.'</th>';
+				$output .= '<th style="background: #91b64f;">'.$configs.'</th>';
 			}
 		}
 		$output .= '</tr>';
 
-		foreach($inputs as $id => $input) {
+		reset($output_fields);
+		foreach($rawinputs as $id => $input) {
 			$output .= '<tr>';
 			foreach($output_fields as $field => $configs) {
-				if(is_array($configs)) {
+				$output_td_style = '';
+				if(is_array($configs) && !is_null($configs['source'])) {
 					if(is_array($configs['source'])) {
 						$source_data = '';
 						foreach($configs['source'] as $source) {
@@ -115,8 +139,45 @@ if($core->input['authCode'] == AUTHCODE) {
 						$output_value = $input[$configs['source']][$configs['attribute']];
 					}
 
+					if(empty($output_value) || strstr($output_value, 'Orkila')) {
+						if($field == 'supplier') {
+							$product = new Products($db->fetch_field($db->query('SELECT localId FROM integration_mediation_products WHERE foreignSystem=3 AND foreignName="'.$input['product']['name'].'"'), 'localId'));
+							if(!is_object($product)) {
+								$product = new Products($db->fetch_field($db->query('SELECT pid FROM products WHERE name="'.$input['product']['name'].'"'), 'pid'));
+							}
+							$output_value = $product->get_supplier()->get()['companyNameShort'];
+							if(empty($output_value)) {
+								$output_value = $product->get_supplier()->get()['companyName'];
+							}
+							$input['supplier']['name'] = $input['supplier']['value'] = $output_value;
+							$input['supplier']['c_bpartner_id'] = md5($output_value);
+						}
+					}
+
+					if(in_array($field, array_keys($summary_categories))) {
+						foreach($summary_categories as $category => $attribute) {
+							$summaries[$category][$input[$category][$attribute]]['name'] = $input[$category]['value'];
+						}
+					}
+
+					if(in_array($field, $summary_reqinfo)) {
+						foreach($summary_categories as $category => $attribute) {
+							if(in_array($field, array('range1qty', 'range2qty', 'range3qty', 'range4qty', 'range5qty'))) {
+								$summaries[$category][$input[$category][$attribute]][$field][$input['product']['uom']['uomsymbol']] += $output_value;
+							}
+							else {
+								$summaries[$category][$input[$category][$attribute]][$field] += $output_value;
+							}
+						}
+					}
+
+					if(in_array($field, $total_types)) {
+						$totals[$field] += $output_value;
+					}
+
 					if($configs['numformat'] == true) {
 						$output_value = number_format($output_value, $report_options['roundto'], '.', ' ');
+						$output_td_style = ' text-align: right;';
 					}
 
 					if($configs['isdate'] == true) {
@@ -131,28 +192,142 @@ if($core->input['authCode'] == AUTHCODE) {
 							$output_value = $output_valueobj->format($core->settings['dateformat']);
 						}
 					}
-					$output_style = '';
-					if(isset($configs['style'])) {
-						$output_style = ' style="'.$configs['style'].'"';
+
+					if(isset($configs['styles'])) {
+						$output_td_style .= $configs['styles'];
 					}
-					$output .= '<td '.$output_style.'>'.$output_value.'</td>';
+
+					if(!in_array($field, $maintable_hiddencols)) {
+						$output .= '<td style="border: 1px solid #CCC; '.$output_td_style.'">'.$output_value.'</td>';
+					}
 					unset($output_value);
 				}
 				else {
 					switch($field) {
 						case 'cost':
-							$output .= '<td>'.number_format(array_sum($input['entries']['cost']), $report_options['roundto'], '.', ' ').'</td>';
+							$output_value = array_sum($input['entries']['cost']);
+							if(in_array($field, $summary_reqinfo)) {
+								foreach($summary_categories as $category => $attribute) {
+									$summaries[$category][$input[$category][$attribute]][$field] += $output_value;
+								}
+							}
+
+							if(in_array($field, $total_types)) {
+								$totals[$field] += $output_value;
+							}
+
+							$output .= '<td style="border: 1px solid #CCC; text-align: right;">'.number_format($output_value, $report_options['roundto'], '.', ' ').'</td>';
 							break;
 						case 'quantity':
-							$output .= '<td>'.number_format(array_sum($input['entries']['qty']), $report_options['roundto'], '.', ' ').'</td>';
+							$output_value = array_sum($input['entries']['qty']);
+							if(in_array($field, $summary_reqinfo)) {
+								foreach($summary_categories as $category => $attribute) {
+									$summaries[$category][$input[$category][$attribute]][$field][$input['product']['uom']['uomsymbol']] += $output_value;
+								}
+							}
+
+							if(in_array($field, $total_types)) {
+								$totals[$field] += $output_value;
+							}
+							$output .= '<td style="border: 1px solid #CCC; text-align: right;">'.number_format($output_value, $report_options['roundto'], '.', ' ').'</td>';
 							break;
 					}
 				}
 			}
 			$output .= '</tr>';
 		}
+		$output .= '<tr>';
+		foreach($output_fields as $field => $configs) {
+			if(in_array($field, $maintable_hiddencols)) {
+				continue;
+			}
+			$output_value = $totals[$field];
+			if(is_numeric($output_value)) {
+				$output_value = number_format($output_value, $report_options['roundto'], '.', ' ');
+			}
+			$output .= '<td style="border: 1px solid #CCC; font-weight: bold; text-align: right;">'.$output_value.'</td>';
+		}
+		$output .= '</tr>';
 		$output .= '</table>';
-		echo $output;
+		/* Parse Summaries - Start */
+		if(is_array($summaries)) {
+			foreach($summaries as $category => $category_data) {
+				$output .= '<h3>'.$output_fields[$category]['title'].' Summary</h3>';
+				$output .= '<table width="100%" cellspacing="0" cellpadding="5" style="border: 1px solid #CCC; font-size: 10px;" border="0">';
+				$output .= '<tr><th style="background: #91b64f;">'.$output_fields[$category]['title'].'</th>';
+				foreach($summary_reqinfo as $reqinfo) {
+					$output .= '<th style="background: #91b64f;">'.$output_fields[$reqinfo]['title'].'</th>';
+				}
+				unset($out_field_title);
+				$output .= '</tr>';
+
+				${$summary_order_attr} = array();
+				foreach($category_data as $cat_data_key => $cat_data_row) {
+					${$summary_order_attr}[$cat_data_key] = $cat_data_row[$summary_order_attr];
+				}
+				array_multisort(${$summary_order_attr}, SORT_DESC, $category_data);
+
+				foreach($category_data as $cat_data_row) {
+					$output .= '<tr>';
+					foreach($cat_data_row as $output_key => $output_value) {
+						$output_td_style = '';
+						if(is_array($output_value)) {
+							$output_values = $output_value;
+							$output_value = '';
+							foreach($output_values as $output_key_temp => $output_value_temp) {
+								if($output_fields[$output_key]['numformat'] == true) {
+									$output_value_temp = number_format($output_value_temp, $report_options['roundto'], '.', ' ');
+									$output_td_style = ' text-align: right;';
+								}
+								$output_value .= $output_value_temp.' '.$output_key_temp.'<br />';
+							}
+						}
+						else {
+							if($output_fields[$output_key]['numformat']) {
+								if($output_fields[$output_key]['numformat'] == true) {
+									$output_value = number_format($output_value, $report_options['roundto'], '.', ' ');
+									$output_td_style = ' text-align: right;';
+								}
+							}
+						}
+
+						if(isset($output_fields[$output_key]['styles'])) {
+							$output_td_style .= $output_fields[$output_key]['styles'];
+						}
+						$output .= '<td style="border: 1px solid #CCC;'.$output_td_style.'">'.$output_value.'</td>';
+					}
+					$output .= '</tr>';
+				}
+				$output .= '</table>';
+			}
+		}
+
+		/* Parse Summaries - END */
+		$message = '<html><head><title>Stock Aging Report</title></head><body>';
+		$message .= $output;
+		$email_data = array(
+				'from_email' => $core->settings['maileremail'],
+				'from' => 'OCOS Mailer',
+				'subject' => 'Stock Aging Report - '.$affiliate['name'].' - Week '.$date_info['week'],
+				'message' => $message
+		);
+
+		$email_data['to'][] = $affiliateobj->get_generalmanager()->get()['email'];
+		$email_data['to'][] = $affiliateobj->get_supervisor()->get()['email'];
+
+		if(isset($affiliates_addrecpt[$affid])) {
+			foreach($affiliates_addrecpt[$affid] as $uid) {
+				$adduser = new Users($uid);
+				$email_data['to'][] = $adduser->get()['email'];
+			}
+		}
+		
+		
+		$email_data['to'] = array();
+		$email_data['to'][] = 'christophe.sacy@orkila.com';
+		//print_r($email_data);
+		$mail = new Mailer($email_data, 'php');
+		unset($message);
 	}
 }
 ?>
