e0fd17c1d171398368d301a74bb0e5a2b4d2a29b
zaher.reda <zaher.reda@orkila.com>
2014-02-20 16:06:51 +0200

    Imporved & fixed issues in improt segments script

--- tools/importsegments.php
+++ tools/importsegments.php
@@ -12,7 +12,7 @@ require '../inc/init.php';
 $core->input['delimiter'] = ';';
 
 
-$csv_file = new CSV('Arranged segments test.csv', 1, true, $core->input['delimiter']);
+$csv_file = new CSV('Arranged segments test2.csv', 1, true, $core->input['delimiter']);
 $csv_file->readdata_file();
 $data_header = $csv_file->get_header();
 $data = $csv_file->get_data();
@@ -21,10 +21,10 @@ $headerval = implode('<br>', $data_header);
 
 // define tables to be imported to
 $tables = array(
-		'productsegments' => array('pk' => 'psid', 'identifier' => 'title', 'headerval' => 'Segment', 'toinsert' => array('title' => 'Segment', 'titleAbbr')),
-		'segmentapplications' => array('pk' => 'psaid', 'foreignid' => 'psid', 'identifier' => 'name', 'title' => 'title', 'headerval' => 'Application', 'toinsert' => array('psid', 'name', 'title' => 'Application', 'createdBy', 'createdOn')),
-		'chemicalfunctions' => array('pk' => 'cfid', 'identifier' => 'name', 'title' => 'title', 'headerval' => 'Functional Property', 'toinsert' => array('name', 'title' => 'Functional Property', 'createdBy', 'createdOn')),
-		//'segapplicationfunctions' => array('pk' => 'safid', 'toinsert' => array('cfid', 'psaid', 'createdBy', 'createdOn')),
+		'productsegments' => array('pk' => 'psid', 'identifier' => 'title', 'checkattr' => 'Segment', 'headerval' => 'Segment', 'toinsert' => array('title' => 'Segment', 'titleAbbr')),
+		'segmentapplications' => array('pk' => 'psaid', 'foreignid' => 'psid', 'identifier' => 'name', 'title' => 'title', 'checkattr' => 'Application', 'checkfor' => array('psid'), 'headerval' => 'Application', 'toinsert' => array('psid', 'name', 'title' => 'Application', 'createdBy', 'createdOn')),
+		'chemicalfunctions' => array('pk' => 'cfid', 'identifier' => 'name', 'title' => 'title', 'checkattr' => 'Functional Property', 'headerval' => 'Functional Property', 'toinsert' => array('name', 'title' => 'Functional Property', 'createdBy', 'createdOn')),
+		'segapplicationfunctions' => array('pk' => 'safid', 'identifier' => 'psaid', 'checkattr' => 'psaid', 'checkfor' => array('cfid'), 'toinsert' => array('cfid', 'psaid', 'createdBy', 'createdOn'))
 );
 
 
@@ -41,10 +41,11 @@ if(is_array($tables)) {
 }
 
 if(is_array($data)) {
-	foreach($data as $value) {
+	foreach($data as $rownum => $value) {
+		echo '<br /><strong>Row '.$rownum.'</strong><br />';
 		foreach($tables as $table => $table_config) {
-			echo'<hr>';
-			//check if fiel name exist get  his id else insert it
+			$found_item = null;
+			echo '=> <em>Attempting to insert into '.$table.'</em><br />';
 			$where_segment = '';
 			if($table == 'productsegments') {
 				$oldpsid = $db->fetch_field($db->query("SELECT psid as psid from productsegments WHERE title='".$value[$table_config['headerval']]."'"), 'psid');
@@ -54,67 +55,61 @@ if(is_array($data)) {
 				$where_segment = " ".$table_config['foreignid'].'!='.$oldpsid." ";
 			}
 
-
-			if(value_exists($table, $table_config['identifier'], $value[$table_config['headerval']])) {
-				$tables_fields_values[$table][$table_config['localid']] = $db->fetch_field($db->query("SELECT ".$table_config['pk']." AS localid  FROM ".Tprefix."$table WHERE ".$table_config[identifier]." ='{$value[$table_config['headerval']]}'"), 'localid');
-				// echo '  data exist take primary key  '.$tables_fields_values[$table][$table_config['localid']];
-				$foreign_ids[$table_config['pk']] = $tables_fields_values[$table][$table_config['localid']];
+			$check_query_where = '';
+			if(isset($table_config['checkfor']) && is_array($table_config['checkfor'])) {
+				foreach($table_config['checkfor'] as $attr) {
+					$check_query_where .= $attr.'="'.$value[$attr].'"';
+				}
 			}
-			else {  
-				//check if segment application do not exist for this segmen to avoid duplications
-				if(!value_exists($table, $table_config['identifier'], $value[$table_config['headerval']], $where_segment)) {
-					foreach($tables[$table]['toinsert'] as $fieldkey => $fieldval) {
-						switch($fieldval) {
-							case 'name':
-								$value[$fieldval] = $value[$table_config['headerval']];  //str_replace(' ', '', $value[$table_config['headerval']]);
-								break;
-							case 'createdOn':
-								$value[$fieldval] = TIME_NOW;
-								break;
-							case 'createdBy':
-								$value[$fieldval] = $core->user['uid'];
-								break;
-						}
+			
+			if(value_exists($table, $table_config['identifier'], $value[$table_config['checkattr']], $check_query_where)) {
+				$found_item = true;
+			}
 
-						if(is_numeric($fieldkey)) {
-							$fieldkey = $fieldval;
-						}
-						$datainsert[$fieldkey] = $value[$fieldval];
-						print_r($datainsert);
-						echo'<hr>';
+			if($table_config['ignorecheck'] === true) {
+				$found_item = false;
+			}
+
+			if($found_item == true) {
+				$tables_fields_values[$table][$table_config['localid']] = $db->fetch_field($db->query("SELECT ".$table_config['pk']." AS localid  FROM ".Tprefix."$table WHERE ".$table_config[identifier]." ='{$value[$table_config['headerval']]}'"), 'localid');
+				echo '==> '.$value[$table_config['headerval']].' already exists (ID='.$tables_fields_values[$table][$table_config['localid']].')<br />';
+				$value[$table_config['pk']] = $tables_fields_values[$table][$table_config['localid']];
+			}
+			else {
+				//check if segment application do not exist for this segmen to avoid duplications
+				foreach($tables[$table]['toinsert'] as $fieldkey => $fieldval) {
+					switch($fieldval) {
+						case 'name':
+							$value[$fieldval] = $value[$table_config['headerval']];  //str_replace(' ', '', $value[$table_config['headerval']]);
+							break;
+						case 'createdOn':
+							$value[$fieldval] = TIME_NOW;
+							break;
+						case 'createdBy':
+							$value[$fieldval] = $core->user['uid'];
+							break;
 					}
 
-					//$db->insert_query($table, $datainsert);
-					/*take the  foreing id (cfid,psaid upon first inser)*/
-					$tables_fields_values[$table][$table_config['localid']] = $db->fetch_field($db->query("SELECT ".$table_config['pk']." AS localid  FROM ".Tprefix."$table WHERE ".$table_config[identifier]." ='{$value[$table_config['headerval']]}'"), 'localid');
-					$foreign_ids[$table_config['pk']] = $tables_fields_values[$table][$table_config['localid']];
-					unset($datainsert);
-					//$value[$table_config['pk']] = $db->last_id();
+					if(is_numeric($fieldkey)) {
+						$fieldkey = $fieldval;
+					}
+					
+					$datainsert[$fieldkey] = $value[$fieldval];
 				}
+
+				$db->insert_query($table, $datainsert);
+				$value[$table_config['pk']] = $db->last_id();
+				echo '==> '.$value[$table_config['headerval']].' was inserted <br />';
+				print_r($datainsert);
+				echo '<br />';
+	
+				$tables_fields_values[$table][$table_config['localid']] = $db->fetch_field($db->query("SELECT ".$table_config['pk']." AS localid  FROM ".Tprefix."$table WHERE ".$table_config[identifier]." ='{$value[$table_config['headerval']]}'"), 'localid');
+				$foreign_ids[$table_config['pk']] = $tables_fields_values[$table][$table_config['localid']];
+				unset($datainsert);
 			}
-			echo'<br>';
 
-			$segarr = array('psaid' => $foreign_ids[psaid],
-					'cfid' => $foreign_ids[cfid],
-					'createdOn' => TIME_NOW,
-					'createdBy' => $core->user['uid']
-			);
-
-			echo'<hr>';
-			if(!empty($segarr['psaid']) && !empty($segarr['cfid'])) {
-
-				if(!value_exists('segapplicationfunctions', 'cfid', $segarr['cfid'], ' psaid='.$segarr['psaid'])) {
-					//echo ' insert into segapplicationfunctions psaid='.$segarr['psaid'].'cfid ='.$segarr[cfid];
-					$db->insert_query('segapplicationfunctions', $segarr);
-					unset($segarr);
-				}
-			}
-			echo'<hr>';
 		}
 	}
 }
 
-
-//get the data
-//do the checking
 ?>
