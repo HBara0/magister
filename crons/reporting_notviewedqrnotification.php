222ad38581ddcac2f3206a3fee9d7d5b6d0c3fab
zaher.reda <zaher.reda@orkila.com>
2014-03-27 12:22:46 +0200

    Fixed and optimized cron to send notifications about not read QR

--- crons/reporting_notviewedqrnotification.php
+++ crons/reporting_notviewedqrnotification.php
@@ -12,33 +12,51 @@ if($_REQUEST['authkey'] == 'kia5ravb$op09dj4a!xhegalhj') {
 	$lang = new Language('english');
 	$lang->load('messages');
 
-	$qreport_objs = ReportingQr::get_reports();
-//	WHERE ".TIME_NOW." > ( sentOn  ".+(strtotime('+1 month')-10).") 
-	foreach($qreport_objs as $qreport_obj) {
-		$reportdata = $qreport_obj->get();  //At least one of the external recipients has not seen it.
-		$recipients_query = $db->query("SELECT * FROM ".Tprefix."reporting_qrrecipients
-									 WHERE reportIdentifier='".$db->escape_string($reportdata['identifier'])."'
-									AND rqrrid NOT IN(SELECT rqrrid FROM  ".Tprefix."reporting_qrrecipients_views)");
-		if($db->num_rows($recipients_query) > 0) {
-			$reportdata['supplieraudit'] = $qreport_obj->get_report_supplier_audits($reportdata['spid']);
-			$reportdata['supplier'] = $qreport_obj->get_report_supplier()['companyName'];
-			/* send each supplier auditor */
-			$body_message = 'Q'.$reportdata['quarter'].' '.$reportdata['year'].'-'.$reportdata['supplier'].'</br>';
+	$offset = 20;
+
+	$quarter = currentquarter_info();
+
+	$recipients_query = $db->query("SELECT * FROM ".Tprefix."reporting_qrrecipients rqr
+									 WHERE rqr.reportIdentifier IN (SELECT r.identifier FROM reports r WHERE r.year={$quarter[year]} AND r.quarter={$quarter[quarter]} AND r.status=1)
+									AND (sentOn+(".($offset * 24 * 60 * 60).") BETWEEN ".strtotime('today')." AND ".strtotime('tomorrow -1 second').")
+									AND NOT EXISTS (SELECT rqv.rqrrid FROM ".Tprefix."reporting_qrrecipients_views rqv WHERE rqv.rqrrid=rqr.rqrrid)
+									AND rpid IS NOT NULL");
+
+	if($db->num_rows($recipients_query) > 0) {
+		while($view = $db->fetch_assoc($recipients_query)) {
+			$rid = $db->fetch_field($db->query("SELECT rid FROM ".Tprefix."reports WHERE identifier='".$db->escape_string($view['reportIdentifier'])."'"), 'rid');
+
+			$report = new ReportingQr(array('rid' => $rid));
+			$reportdata = $report->get();
+			$supplier_obj = $report->get_report_supplier(true);
+			$reportdata['supplier'] = $supplier_obj->get()['companyName'];
+
+			$auditor_obj = new Users($view['sentBy']);
+			$auditor = $auditor_obj->get();
+
+			$represenative = new Representatives($view['rpid']);
+			$messages[$auditor['uid']]['info'] = $auditor;
+			$messages[$auditor['uid']]['message'] .= 'Q'.$reportdata['quarter'].'/'.$reportdata['year'].' - '.$reportdata['supplier'].' - '.$represenative->get()['name'].' ('.$represenative->get()['email'].')</br>';
 		}
-		if(empty($body_message)) {
-			continue;
-		}
-		$email_data = array(
-				'to' => $reportdata['supplieraudit']['email'],
-				'from_email' => $core->settings['maileremail'],
-				'from' => 'OCOS Mailer',
-				'subject' => $lang->report_notviewdsubject,
-				'message' => '<strong>'.$lang->report_notviewdmessage.'</strong><br />'.$body_message
-		);
-		$body_message = '';
-		$mail = new Mailer($email_data, 'php');
-		if($mail->get_status() === true) {
-			$log->record('reporting_reportnotviewd', array('to' => $reportdata['supplieraudit']['email']));
+	}
+
+	if(is_array($messages)) {
+		foreach($messages as $uid => $auditor) {
+			if(empty($auditor['message'])) {
+				continue;
+			}
+			$mailer = new Mailer();
+			$mailer = $mailer->get_mailerobj();
+			$mailer->set_to($auditor['info']['email']);
+			$mailer->set_from(array('name' => 'OCOS Mailer', 'email' => $core->settings['maileremail']));
+			$mailer->set_subject($lang->sprint($lang->reporting_notviewedqrnote_subj, $offset));
+			$mailer->set_message($lang->sprint($lang->reporting_notviewedqrnote_body, $offset, $auditor['message']));
+			
+			$mailer->send();
+
+			if($mailer->get_status() === true) {
+				$log->record('reporting_reportnotviewd', array('to' => $reportdata['supplieraudit']['email']));
+			}
 		}
 	}
 }
