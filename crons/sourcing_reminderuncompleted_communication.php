66b0018f6c775cf3be402c7a5620a02241f4802a
tony.assaad <tony.assaad@orkila.com>
2014-04-04 14:45:32 +0300

    -adding affiliate name made for the  contact history.
    -link to the  history section in the supplier profile.

--- crons/sourcing_reminderuncompleted_communication.php
+++ crons/sourcing_reminderuncompleted_communication.php
@@ -26,12 +26,12 @@ if($_REQUEST['authkey'] == 'ac43bghy!h4k23jh4k2_3h4k23jh') {
 		foreach($incompleted_communications as $uid => $communications) {
 			$body_message = '<ul>';
 			foreach($incompleted_communications[$uid] as $invitationdetails) {
-
+				$aff_obj = new Affiliates($invitationdetails['affid']);
 				$entity_obj = new Entities($invitationdetails['ssid']);
 				$invitationdetails['supplier'] = $entity_obj->get();
-				$body_message.='<li>'.$invitationdetails['supplier']['companyName'].' Made on '.date('M d  Y ', $invitationdetails['date']);
-				$body_message .= '<p>'.$invitationdetails['description'].'</p>';
-				$body_message .= '<p>'.$invitationdetails['application'].'</p>';
+				$body_message.='<li><a target="_blank" href="'.DOMAIN.'/index.php?module=sourcing/supplierprofile&id='.$invitationdetails['supplier']['eid'].'#historybrief_'.$invitationdetails['sschid'].' ">'.$invitationdetails['supplier']['companyName'].'</a> Made on '.date('M d  Y ', $invitationdetails['date']).' - '.$aff_obj->get()['name'];
+				$body_message .='<p><span><strong>'.$lang->description.' </strong>: '.$invitationdetails['description'].'</span></p>';
+				$body_message .= '<p><span><strong>'.$lang->application.'</strong> : '.$invitationdetails['application'].'</span></p>';
 
 				$body_message.='</li>';
 				if(empty($body_message)) {
@@ -45,14 +45,14 @@ if($_REQUEST['authkey'] == 'ac43bghy!h4k23jh4k2_3h4k23jh') {
 
 			/* Prepare the email_data array to pass the argument to the mail object */
 			$email_data = array(
-					'to' => 'tony.assaad@orkila.com',
+					'to' => $invitationdetails['user']['email'],
 					'from_email' => $core->settings['maileremail'],
 					'from' => 'OCOS Mailer',
 					'subject' => $lang->uncompletedsubject,
 					'message' => $lang->sprint($lang->uncompletedcommunication, $invitationdetails['user']['displayName']).'</br>'.$body_message.''
 			);
-			 	$email_data['cc'] = 'sourcing@orkila.com';
-
+			$email_data['cc'] = 'sourcing@orkila.com';
+	
 			$mail = new Mailer($email_data, 'php');
 			if($mail->get_status() === true) {
 				$log->record('sourcing_suppliers_contacthist', array('to' => $invitationdetails['email']));
