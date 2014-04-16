e2c411aa4ccff3e4f06100316446718c54d0125d
unknown <tony.assaad@L-PROBOOK450-2.orkilalb.local>
2014-04-16 09:56:03 +0300

    -formatting source code.

--- inc/LeavesMessages_class.php
+++ inc/LeavesMessages_class.php
@@ -11,70 +11,69 @@
  * @author tony.assaad
  */
 class LeavesMessages {
-	//put your code here 
+    //put your code here 
 
-	private $errorcode = 0; //0=No errors;1=Subject missing;2=Entry exists;3=Error saving;4=validation violation
-	private $leavemessage = array();
+    private $errorcode = 0; //0=No errors;1=Subject missing;2=Entry exists;3=Error saving;4=validation violation
+    private $leavemessage = array();
 
-	public function __construct($leavemessage = array(), $simple = true) {
-		global $db;
-		if(!is_array($leavemessage) && !empty($leavemessage)) {
-			$this->leavemessage = $this->read($leavemessage, $simple);
-		}
-		else {
-			if(isset($leavemessage['lmid']) && !empty($leavemessage['lmid'])) {
-				$this->leavemessage = $this->read($leavemessage['lmid'], $simple);
-			}
-		}
-	}
+    public function __construct($leavemessage = array(), $simple = true) {
+        global $db;
+        if(!is_array($leavemessage) && !empty($leavemessage)) {
+            $this->leavemessage = $this->read($leavemessage, $simple);
+        }
+        else {
+            if(isset($leavemessage['lmid']) && !empty($leavemessage['lmid'])) {
+                $this->leavemessage = $this->read($leavemessage['lmid'], $simple);
+            }
+        }
+    }
 
-	private function read($id, $simple = true) {
-		global $db;
+    private function read($id, $simple = true) {
+        global $db;
 
-		if(empty($id)) {
-			return false;
-		}
-		$query_select = '*';
-		if($simple == true) {
-			$query_select = 'lmid, lid,uid,inReplyto,message';
-		}
+        if(empty($id)) {
+            return false;
+        }
+        $query_select = '*';
+        if($simple == true) {
+            $query_select = 'lmid, lid,uid,inReplyto,message';
+        }
 
-		return $db->fetch_assoc($db->query("SELECT {$query_select} FROM ".Tprefix."leaves_messages WHERE lmid=".$db->escape_string($id)));
-	}
+        return $db->fetch_assoc($db->query("SELECT {$query_select} FROM ".Tprefix."leaves_messages WHERE lmid=".$db->escape_string($id)));
+    }
 
-	public function Get_Inreplyto() {
-		return new LeavesMessages($this->leavemessage['inReplyto']);  /* Get the reply messaage id  of the current message object */
-	}
+    public function Get_Inreplyto() {
+        return new LeavesMessages($this->leavemessage['inReplyto']);  /* Get the reply messaage id  of the current message object */
+    }
 
-	public function get_viewpermissions() {
+    public function get_viewpermissions() {
 
-		switch($this->leavemessage['viewPermission']) {
-			case'private':
-				$inreply_obj = $this->Get_Inreplyto();
-				if(is_object($inreply_obj)) {  //requester 
-					$users_permission['Inreplyto'] = $this->Get_Inreplyto()->get_user()[uid];
-				}
-				print_r($users_permission);
-				$users_permission['requester'] = $this->leavemessage['uid'];
-				if($core->user[uid] == $this->leavemessage['uid']) {
-					
-				}
-				//requester and the person setting the message can it
+        switch($this->leavemessage['viewPermission']) {
+            case'private':
+                $inreply_obj = $this->Get_Inreplyto();
+                if(is_object($inreply_obj)) {  //requester 
+                    $users_permission['Inreplyto'] = $this->Get_Inreplyto()->get_user()[uid];
+                }
+                print_r($users_permission);
+                $users_permission['requester'] = $this->leavemessage['uid'];
+                if($core->user[uid] == $this->leavemessage['uid']) {
+                    
+                }
+                //requester and the person setting the message can it
 
-				break;
-			case'public':
-				break;
-			case 'limited':
-				break;
-		}
-	}
+                break;
+            case'public':
+                break;
+            case 'limited':
+                break;
+        }
+    }
 
 //if permsiion private  get uid  of sendgin mesage 
-	public function create_message($data = array()) {
-		global $db;
-		print_R($data);
-		//read emai header 
-
+    public function create_message($data = array()) {
+        global $db;
+        print_R($data);
+        //read emai header 
 //		$message_data = array('lid' => $leaveid,
 //		'uid' =>,
 //		'inRepyTo' =>,
@@ -83,24 +82,24 @@ class LeavesMessages {
 //		'viewPermission' =>,
 //		'createdOn' => TIME_NOW);
 
-		echo 'create messaage';
-		//$db->insert_query($table, $message_data, $options);
-	}
+        echo 'create messaage';
+        //$db->insert_query($table, $message_data, $options);
+    }
 
-	public function read_message() {
-		return $this->leavemessage['message'];
-	}
+    public function read_message() {
+        return $this->leavemessage['message'];
+    }
 
-	public function get_leave() {
-		return new Leaves($this->leavemessage['lid']);
-	}
+    public function get_leave() {
+        return new Leaves($this->leavemessage['lid']);
+    }
 
-	public function get_user() {
-		return new Users($this->leavemessage['uid']);
-	}
+    public function get_user() {
+        return new Users($this->leavemessage['uid']);
+    }
 
-	public function get() {
-		return $this->leavemessage;
-	}
+    public function get() {
+        return $this->leavemessage;
+    }
 
 }
--- inc/languages/english/messages.lang.php
+++ inc/languages/english/messages.lang.php
@@ -137,7 +137,7 @@ $lang['yournewpassword'] = 'Your New OCOS Account Password';
 $lang['pendingchemrequestssubject'] = 'You have pending chemcial requests';
 $lang['pendingchemrequestsbody'] = 'The following chemical requests are still pending:';
 $lang['report_notviewdsubject'] = 'Not viewed reports Notifications';
-$lang['report_notviewdmessage'] = 'The following QR have not been viewed by any of the recipients: '
+$lang['report_notviewdmessage'] = 'The following QR have not been viewed by any of the recipients: ';
 $lang['uncompletedcommunication'] = ' You Have Not completed Your communication With ';
 $lang['uncompletedsubject'] = 'Uncompleted Sourcing Communication ';
 ?>
