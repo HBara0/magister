fab66ebc9ac19ca6fbd529e0e04f9b2ae3fdae93
tony.assaad <tony.assaad@orkila.com>
2014-05-14 13:29:55 +0300

    Correct   in  parse_segments_byuser in leavetype
    -correct the core->input to core->user.

--- inc/LeaveTypes_class.php
+++ inc/LeaveTypes_class.php
@@ -203,7 +203,7 @@ class Leavetypes {
         if($this->leavetype['isBusiness'] == 1) {
             /* only we get the segments of  selected user (core user) */
             if(!is_object($user)) {
-                $user_obj = new Users($core->input['uid']);
+                $user_obj = new Users($core->user['uid']);
             }
             $user_segmentsobjs = $user_obj->get_segments();
             if(is_array($user_segmentsobjs)) {
