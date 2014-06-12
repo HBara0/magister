1d7cfc164ad5abef857d25728c267806509fd371
zaher.reda <zaher.reda@orkila.com>
2014-06-12 13:48:47 +0300

    Fix issues, optimize, apply security enhancement, and add new search type for both pc and c in search.php

--- search.php
+++ search.php
@@ -15,28 +15,29 @@ if($core->input['type'] == 'quick') {
     if(isset($core->input['filter'])) {
         $dofilter = true;
         if(isset($core->input['rid']) && !empty($core->input['rid'])) {
-            $report_data = $db->fetch_array($db->query("SELECT affid, spid FROM ".Tprefix."reports WHERE rid='".$db->escape_string($core->input['rid'])."'"));
+            $report_data = $db->fetch_array($db->query('SELECT affid, spid FROM '.Tprefix.'reports WHERE rid='.intval($core->input['rid'])));
         }
         else {
             if(isset($core->input['cid']) && !empty($core->input['cid'])) {
-                $customer_filter = "er.eid='".$db->escape_string($core->input['cid'])."'";
+                $customer_filter = 'er.eid='.intval($core->input['cid']);
             }
 
             if(isset($core->input['spid']) && !empty($core->input['spid'])) {
                 if(is_array($core->input['spid'])) {
+                    $core->input['spid'] = array_map(intval, $core->input['spid']);
                     if($core->input['for'] == 'product') {
-                        $supplier_filter = "spid IN ('".$db->escape_string(implode(',', $core->input['spid']))."')";
+                        $supplier_filter = 'spid IN ('.implode(',', $core->input['spid']).')';
                     }
                     else {
-                        $supplier_filter = "er.eid IN ('".$db->escape_string(implode(',', $core->input['spid']))."')";
+                        $supplier_filter = 'er.eid IN ('.implode(',', $core->input['spid']).')';
                     }
                 }
                 else {
                     if($core->input['for'] == 'product') {
-                        $supplier_filter = "spid='".$db->escape_string($core->input['spid'])."'";
+                        $supplier_filter = 'spid='.intval($core->input['spid']);
                     }
                     else {
-                        $supplier_filter = "er.eid='".$db->escape_string($core->input['spid'])."'";
+                        $supplier_filter = 'er.eid='.intval($core->input['spid']);
                     }
                 }
             }
@@ -44,18 +45,21 @@ if($core->input['type'] == 'quick') {
     }
 
     if(isset($core->input['for'])) {
-        if($core->input['for'] == 'potentialsupplier' || $core->input['for'] == 'potentialcustomer') {
-            if($core->input['for'] == 'potentialcustomer') {
-                $type = 'pc';
-            }
+        if($core->input['for'] == 'potentialcustomer') {
             $table = 'entities';
             $attributes = array('companyName', 'companyNameAbbr');
             $key_attribute = 'eid';
             $select_attributes = array('companyName');
             $order = array('by' => 'companyName', 'sort' => 'ASC');
-            if(!empty($ispotential)) {
-                $extra_where .= '  type="'.$type.'" AND isPotential="'.$ispotential.'"';
-            }
+            $extra_where .= ' type="pc"';
+        }
+        if($core->input['for'] == 'allcustomertypes') {
+            $table = 'entities';
+            $attributes = array('companyName', 'companyNameAbbr');
+            $key_attribute = 'eid';
+            $select_attributes = array('companyName');
+            $order = array('by' => 'companyName', 'sort' => 'ASC');
+            $extra_where .= ' type IN ("pc", "c")';
         }
         if($core->input['for'] == 'supplier' || $core->input['for'] == 'customer' || $core->input['for'] == 'competitorsupp' || $core->input['for'] == 'competitortradersupp' || $core->input['for'] == 'competitorproducersupp') {
             if($core->input['for'] == 'supplier') {
@@ -67,19 +71,20 @@ if($core->input['type'] == 'quick') {
             }
             elseif($core->input['for'] == 'competitorsupp') {
                 $type = 'cs';
-                $extra_where = 'supplierType="trader" OR supplierType="producer" OR supplierType="" ';
+                $extra_where = 'supplierType="t" OR supplierType="p" OR supplierType="b"';
             }
             elseif($core->input['for'] == 'competitortradersupp') {
                 $type = 'cs';
-                $extra_where = 'supplierType="trader"';
+                $extra_where = 'supplierType="t"';
             }
             elseif($core->input['for'] == 'competitorproducersupp') {
                 $type = 'cs';
-                $extra_where = 'supplierType="producer"';
+                $extra_where = 'supplierType="p"';
             }
             else {
                 $type = 'c';
                 if($core->usergroup['canViewAllCust'] == 0) {
+                    $core->user['customers'] = array_map(intval, $core->user['customers']);
                     $inentities = implode(',', $core->user['customers']);
                     $extra_where = 'eid IN ('.$inentities.')';
                     $extra_where = 'eid IN (SELECT affe.eid FROM  affiliatedentities  affe
@@ -130,7 +135,7 @@ if($core->input['type'] == 'quick') {
         }
         elseif($core->input['for'] == 'chemfunctionproducts') {
             if($core->usergroup['canViewAllsupp'] == 0) {
-                $supplier_filter = "spid IN('".implode(',', $core->user['suppliers']['eid'])."')";
+                $supplier_filter = 'spid IN ('.implode(',', $core->user['suppliers']['eid']).')';
             }
             if(!empty($supplier_filter)) {
 //$extra_where = $supplier_filter;
@@ -197,6 +202,7 @@ if($core->input['type'] == 'quick') {
         }
 
         if(isset($core->input['exclude']) && !empty($core->input['exclude'])) {
+            $core->input[exclude] = array_map(intval, $core->input['exclude']);
             if(empty($extra_where)) {
                 $extra_where = "{$key_attribute} NOT IN ({$core->input[exclude]})";
             }
