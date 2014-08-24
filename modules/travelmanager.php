d016429fa0b63d3312a23bc43df16b4d0e7c5dc1
zaher.reda <zaher.reda@orkila.com>
2014-08-24 18:35:25 +0300

    Fix names of list plans files

--- modules/travelmanager.php
+++ modules/travelmanager.php
@@ -3,8 +3,8 @@ $module['name'] = 'travelmanager';
 $module['title'] = $lang->travelmanager;
 $module['homepage'] = 'lookupflights';
 $module['globalpermission'] = 'canUseTravelManager';
-$module['menu'] = array('file' => array('lookupflights', 'viewplan', 'listplan'),
-        'title' => array('lookupflights', 'viewplan', 'listplan'),
-        'permission' => array('canUseTravelManager', 'canUseTravelManager', 'canUseTravelManager')
+$module['menu'] = array('file' => array('plantrip', 'listplan'),
+        'title' => array('plantrip', 'listplans'),
+        'permission' => array('canUseTravelManager', 'canUseTravelManager')
 );
 ?>
