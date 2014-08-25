1d3f3cee7a91059769637ff6ff80a6d72c3d3737
zaher.reda <zaher.reda@orkila.com>
2014-08-25 16:24:31 +0300

    Correct name of class CalendarTaskShares, fix small issues, and fix major security holes

--- inc/CalendarTaskShares_class.php
+++ inc/CalendarTaskShares_class.php
@@ -13,14 +13,14 @@
  *
  * @author tony.assaad
  */
-class CalendarTaskShare extends AbstractClass {
+class CalendarTaskShares extends AbstractClass {
     protected $data = array();
     protected $errorcode = 0;
 
     const PRIMARY_KEY = 'ctsid';
-    const TABLE_NAME = 'calendar_tasks_sharewith';
+    const TABLE_NAME = 'calendar_tasks_shares';
     const DISPLAY_NAME = '';
-    const SIMPLEQ_ATTRS = 'ctsid,ctid,uid';
+    const SIMPLEQ_ATTRS = 'ctsid, ctid, uid';
     const CLASSNAME = __CLASS__;
 
     public function __construct($id = '', $simple = true) {
@@ -44,15 +44,16 @@ class CalendarTaskShare extends AbstractClass {
                 $existing_users = array_keys($existing_users);
                 $users_toremove = array_diff($existing_users, $data);
                 if(!empty($users_toremove)) {
-                    $db->delete_query(self::TABLE_NAME, 'uid IN ('.$db->escape_string(implode(',', $users_toremove)).') AND ctid='.$task_data['ctid']);
+                    $users_toremove = array_map(intval, $users_toremove);
+                    $db->delete_query(self::TABLE_NAME, 'uid IN ('.implode(',', $users_toremove).') AND ctid='.intval($task_data['ctid']));
                     $this->errorcode = 0;
                 }
             }
             foreach($data as $uid) {
                 $task_data['createdBy'] = $core->user['uid'];
                 $task_data['createdOn'] = TIME_NOW;
-                $task_data['uid'] = $core->sanitize_inputs($uid);
-                if(!value_exists(self::TABLE_NAME, 'uid', $uid, ' ctid='.$this->data['ctid'])) {
+                $task_data['uid'] = intval($uid);
+                if(!value_exists(self::TABLE_NAME, 'uid', $uid, 'ctid='.intval($this->data['ctid']))) {
                     $db->insert_query(self::TABLE_NAME, $task_data);
                 }
             }
@@ -90,24 +91,12 @@ class CalendarTaskShare extends AbstractClass {
 
     }
 
-    public function __set($name, $value) {
-        $this->data[$name] = $value;
-    }
-
     public function get_user() {
         return new Users($this->data['uid']);
     }
 
     public function get_task() {
         return new Tasks($this->data['ctid']);
-    }
-
-    public function get_errorcode() {
-        return parent::get_errorcode();
-    }
-
-    public function get() {
-        return parent::get();
     }
 
 }
