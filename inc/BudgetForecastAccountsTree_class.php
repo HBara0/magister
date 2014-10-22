d47276ffa5357dc1ec8a2bba2dd0c77fc05bc401
zaher.reda <zaher.reda@orkila.com>
2014-10-22 16:02:56 +0300

    Fix layout for budget balance sheet

--- inc/BudgetForecastAccountsTree_class.php
+++ inc/BudgetForecastAccountsTree_class.php
@@ -94,14 +94,19 @@ class BudgetForecastAccountsTree extends AbstractClass {
         foreach($items as $id => $item) {
             switch($item->accountLevel) {
                 case 'heading':
-                    $class = 'subtitle';
+                    $class = 'thead';
                     $item->title = ucwords(strtolower($item->title));
+                    $colspan = 2;
                     break;
                 case 'account':
                 case 'Account';
+                    $class = 'subtitle';
+                    $item->title = ucwords(strtolower($item->title));
+                    $colspan = 2;
                     break;
                 default:
                     $class = '';
+                    $colspan = 1;
                     break;
             }
 
@@ -109,7 +114,7 @@ class BudgetForecastAccountsTree extends AbstractClass {
                 $item = self::get_data(array('batid' => $item['batid']));
             }
 
-            $output .= '<tr><td class="'.$class.'" style="padding-left: '.(5 * $depth).'px;">'.$item->title.'</td>';
+            $output .= '<tr><td colspan="'.$colspan.'" class="'.$class.'" style="padding-left: '.(5 * $depth).'px;">'.$item->title.'</td>';
 
             if(method_exists($item, get_children)) {
                 $account_children = $item->get_children();
