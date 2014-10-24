ff5ce5ae554c1d9bf28e6b713c0162bb55450b91
tony.assaad <tony.assaad@orkila.com>
2014-10-24 11:14:13 +0300

    add  desc  Currency in this budget is  Commercial and Administration Expenses

--- modules/budgeting/financialadminexpenses.php
+++ modules/budgeting/financialadminexpenses.php
@@ -67,6 +67,25 @@ if(!isset($core->input['action'])) {
     }
     $budgeting_header .='</tr>';
 
+    /* get main currecny of the affiliate being budgeted */
+
+    $affilaite_obj = new Affiliates($affid);
+    $budget_affiliatecurr = $affilaite_obj->get_country()->get_maincurrency();
+    if(!empty($budget_affiliatecurr)) {
+        $tocurrency = '840'; //usd
+        $currencyto_obj = new Currencies($tocurrency);
+        $currency_to = $currencyto_obj->get()['alphaCode'];
+        $dal_config = array(
+                'operators' => array('fromCurrency' => '=', 'affid' => 'in', 'year' => '='),
+                'simple' => false,
+                'returnarray' => false
+        );
+        $fxrates_obj = BudgetFxRates::get_data(array('fromCurrency' => $budget_affiliatecurr->numCode, 'toCurrency' => $tocurrency, 'affid' => $affid, 'year' => $financialbudget_year,), $dal_config);
+        if(is_object($fxrates_obj)) {
+            $output_currency = '<div class="ui-state-highlight ui-corner-all" style="padding-left: 5px; padding: 5px; margin-top: 10px; margin-bottom: 10px; display: block;"><span><em>'.$lang->sprint($lang->budgcurrdesc, $budget_affiliatecurr->alphaCode).'</em></span></br><em><strong>'.$lang->exchangerate.'</strong></em></br><span>'.$lang->sprint($lang->currrate, $budget_affiliatecurr->alphaCode, $currency_to, $fxrates_obj->rate).'</span></div>';
+        }
+    }
+
     eval("\$budgeting_commercialexpenses = \"".$template->get('budgeting_commercialexpenses')."\";");
     output_page($budgeting_commercialexpenses);
 }
