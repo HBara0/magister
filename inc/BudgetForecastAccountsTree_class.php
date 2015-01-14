4874ec156ed51b9ed4a635a9cc4eeda2cdf74033
zaher.reda <zaher.reda@orkila.com>
2015-01-14 10:13:09 +0200

    Fix bug generating FX rate error when currncies are equal

--- inc/BudgetForecastAccountsTree_class.php
+++ inc/BudgetForecastAccountsTree_class.php
@@ -185,8 +185,8 @@ class BudgetForecastAccountsTree extends AbstractClass {
                                 $callback = function($val) use ($currency) {
                                     return $val->fromCurrency == $currency;
                                 };
-                                $budgetfx = array_filter($options['fxrates'], $callback);
                                 if($finbudget->currency != $options['toCurrency']) {
+                                    $budgetfx = array_filter($options['fxrates'], $callback);
                                     $budgetfx = current($budgetfx);
                                     $amnt = $amnt * $budgetfx->rate;
                                 }
