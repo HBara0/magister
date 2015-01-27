64d1fa85a10a1a95389bdb693299836c99787a1b
tony.assaad <tony.assaad@orkila.com>
2015-01-27 09:30:25 +0200

    add lang files for TM and GP

--- modules/grouppurchase.php
+++ modules/grouppurchase.php
@@ -1,3 +1,4 @@
+
 <?php
 $module['name'] = 'grouppurchase';
 $module['title'] = $lang->grouppurchase;
@@ -5,6 +6,6 @@ $module['homepage'] = 'pricing';
 $module['globalpermission'] = 'canUseGroupPurchase';
 $module['menu'] = array('file' => array('pricing', 'priceslist', 'createforecast', 'generateforecast'),
         'title' => array('priceproduct', 'priceslist', 'createforecast', 'generateforecast'),
-        'permission' => array('grouppurchase_canPrice', 'canUseGroupPurchase', 'grouppurchase_canUpdateForecast', 'grouppurchase_canUpdateForecast')
+        'permission' => array('grouppurchase_canPrice', 'canUseGroupPurchase', 'grouppurchase_canUpdateForecast', 'grouppurchase_canGenerateReports')
 );
 ?>
