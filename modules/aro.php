7789e600b56ff97bbf0a7db8fe811e089c288794
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-02-04 16:53:29 +0200

    Add menu items for manage policies and policies list in Aro module

--- modules/aro.php
+++ modules/aro.php
@@ -3,8 +3,8 @@ $module['name'] = 'aro';
 $module['title'] = $lang->aro;
 $module['homepage'] = 'listwarehouses';
 $module['globalpermission'] = 'canUseAro';
-$module['menu'] = array('file' => array('managewarehousepolicies', 'warehousespolicieslist'),
-        'title' => array('managewarehousepolicies', 'warehousespolicieslist'),
-        'permission' => array('aro_canManageWarehousePolicies', 'aro_canManageWarehousePolicies')
+$module['menu'] = array('file' => array('managewarehousepolicies', 'warehousespolicieslist', 'managepolicies', 'listpolicies'),
+        'title' => array('managewarehousepolicies', 'warehousespolicieslist', 'managepolicies', 'aropolicieslist'),
+        'permission' => array('aro_canManageWarehousePolicies', 'aro_canManageWarehousePolicies', 'aro_canManagePolicies', 'aro_canManagePolicies')
 );
 ?>
