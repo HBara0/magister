b00db6915d33aaff328c957e3a1e06016497d64f
tony.assaad <tony.assaad@orkila.com>
2015-02-04 17:49:49 +0200

    remove  unenecessary exit

--- modules/grouppurchase/createforecast.php
+++ modules/grouppurchase/createforecast.php
@@ -13,7 +13,7 @@ if(!defined('DIRECT_ACCESS')) {
 }
 
 if($core->usergroup['grouppurchase_canUpdateForecast'] == 0) {
-    // error($lang->sectionnopermission);
+    error($lang->sectionnopermission);
 }
 
 if(!$core->input['action']) {
