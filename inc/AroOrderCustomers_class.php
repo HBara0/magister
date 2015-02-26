ae9111641a454d8420dbeaf3f7358a844f94f945
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-02-26 16:11:23 +0200

    Fix issue of saving order customers lines
    -Fix issue of not saving unspecified customer row
    -Remove saving of empty rows

--- inc/AroOrderCustomers_class.php
+++ inc/AroOrderCustomers_class.php
@@ -30,47 +30,50 @@ class AroOrderCustomers extends AbstractClass {
 
     protected function update(array $data) {
         global $db, $core, $log;
-        $data['paymentTermBaseDate'] = strtotime($data['paymentTermBaseDate']);
-        $data_array = array('cid' => $data['cid'],
-                'inputChecksum' => $data['inputChecksum'],
-                'ptid' => $data['ptid'],
-                'aorid' => $data['aorid'],
-                'paymentTermDesc' => $data['paymentTermDesc'],
-                'paymentTermBaseDate' => $data['paymentTermBaseDate'],
-                'modifiedBy' => $core->user['uid'],
-                'modifiedOn' => TIME_NOW,
-        );
-        $query = $db->update_query(self::TABLE_NAME, $data_array, self::PRIMARY_KEY.' = '.intval($this->data[self::PRIMARY_KEY]));
-        if($query) {
-            $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+        if(!$this->validate_requiredfields($data)) {
+            $data['paymentTermBaseDate'] = strtotime($data['paymentTermBaseDate']);
+            $data_array = array('cid' => $data['cid'],
+                    'inputChecksum' => $data['inputChecksum'],
+                    'ptid' => $data['ptid'],
+                    'aorid' => $data['aorid'],
+                    'paymentTermDesc' => $data['paymentTermDesc'],
+                    'paymentTermBaseDate' => $data['paymentTermBaseDate'],
+                    'modifiedBy' => $core->user['uid'],
+                    'modifiedOn' => TIME_NOW,
+            );
+            $query = $db->update_query(self::TABLE_NAME, $data_array, self::PRIMARY_KEY.' = '.intval($this->data[self::PRIMARY_KEY]));
+            if($query) {
+                $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+            }
         }
     }
 
     public function create(array $data) {
         global $db, $core, $log;
-        $required_fields = array('cid');
-        foreach($required_fields as $field) {
-            $data[$field] = $core->sanitize_inputs($data[$field], array('removetags' => true, 'allowable_tags' => '<blockquote><b><strong><em><ul><ol><li><p><br><strike><del><pre><dl><dt><dd><sup><sub><i><cite><small>'));
-            if(is_empty($data[$field]) && $data[$field] != 0) {
-                $this->errorcode = 2;
-                return false;
+        if(!$this->validate_requiredfields($data)) {
+            $required_fields = array('cid');
+            foreach($required_fields as $field) {
+                $data[$field] = $core->sanitize_inputs($data[$field], array('removetags' => true, 'allowable_tags' => '<blockquote><b><strong><em><ul><ol><li><p><br><strike><del><pre><dl><dt><dd><sup><sub><i><cite><small>'));
+                if(is_empty($data[$field]) && $data[$field] != 0) {
+                    $this->errorcode = 2;
+                    return false;
+                }
             }
-        }
-
-        $data['paymentTermBaseDate'] = strtotime($data['paymentTermBaseDate']);
-        $policies_array = array('cid' => $data['cid'],
-                'ptid' => $data['ptid'],
-                'aorid' => $data['aorid'],
-                'paymentTermDesc' => $data['paymentTermDesc'],
-                'paymentTermBaseDate' => $data['paymentTermBaseDate'],
-                'createdBy' => $core->user['uid'],
-                'createdOn' => TIME_NOW,
-        );
-        $query = $db->insert_query(self::TABLE_NAME, $policies_array);
-        if($query) {
-            $this->data[self::PRIMARY_KEY] = $db->last_id();
-            $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
-            $this->errorcode = 0;
+            $data['paymentTermBaseDate'] = strtotime($data['paymentTermBaseDate']);
+            $policies_array = array('cid' => $data['cid'],
+                    'ptid' => $data['ptid'],
+                    'aorid' => $data['aorid'],
+                    'paymentTermDesc' => $data['paymentTermDesc'],
+                    'paymentTermBaseDate' => $data['paymentTermBaseDate'],
+                    'createdBy' => $core->user['uid'],
+                    'createdOn' => TIME_NOW,
+            );
+            $query = $db->insert_query(self::TABLE_NAME, $policies_array);
+            if($query) {
+                $this->data[self::PRIMARY_KEY] = $db->last_id();
+                $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+                $this->errorcode = 0;
+            }
         }
     }
 
@@ -85,6 +88,18 @@ class AroOrderCustomers extends AbstractClass {
         }
         if(!empty($intervals)) {
             return array_sum($intervals / count($intervals));
+        }
+    }
+
+    private function validate_requiredfields(array $data = array()) {
+        if(is_array($data)) {
+            $required_fields = array('cid', 'ptid');
+            foreach($required_fields as $field) {
+                if(empty($data[$field]) && $data[$field] != '0') {
+                    $this->errorcode = 2;
+                    return true;
+                }
+            }
         }
     }
 
