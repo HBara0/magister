38bd6c7ad36b991b692e39a7c42c9f28c88be1c8
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-03-11 12:31:05 +0200

    Create Aro Parties info related class

---
+++ inc/AroPartiesInformation_class.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * Copyright © 2015 Orkila International Offshore, All Rights Reserved
+ *
+ * [Provide Short Descption Here]
+ * $id: AroPartiesInformation_class.php
+ * Created:        @rasha.aboushakra    Mar 10, 2015 | 9:43:04 AM
+ * Last Update:    @rasha.aboushakra    Mar 10, 2015 | 9:43:04 AM
+ */
+
+class AroPartiesInformation extends AbstractClass {
+    protected $data = array();
+    public $errorcode = 0;
+
+    const PRIMARY_KEY = 'apiid';
+    const TABLE_NAME = 'aro_partiesinformation';
+    const DISPLAY_NAME = '';
+    const SIMPLEQ_ATTRS = '*';
+    const CLASSNAME = __CLASS__;
+    const UNIQUE_ATTRS = 'aorid';
+
+    public function __construct($id = '', $simple = true) {
+        parent::__construct($id, $simple);
+    }
+
+    protected function create(array $data) {
+        global $db, $log;
+        $partiesinfo_data = $this->calculate_partiesinfofields($data);
+        $query = $db->insert_query(self::TABLE_NAME, $partiesinfo_data);
+        if($query) {
+            $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+        }
+    }
+
+    protected function update(array $data) {
+        global $db, $log;
+        $partiesinfo_data = $this->calculate_partiesinfofields($data);
+        $query = $db->update_query(self::TABLE_NAME, $partiesinfo_data, self::PRIMARY_KEY.' = '.intval($this->data[self::PRIMARY_KEY]));
+        if($query) {
+            $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+        }
+    }
+
+    private function validate_requiredfields(array $data = array()) {
+        if(is_array($data)) {
+            $required_fields = array('estDateOfShipment', 'shipmentCountry', 'originCountry', 'vendorIncoterms', 'vendorIncotermsDesc', 'vendorPaymentTerm', 'vendorPaymentTermDesc', 'commission');
+            if($data['vendorIsAff'] == 0) {
+                $additionalfields = array('vendorEid', 'intermedAff', 'intermedIncoterms', 'intermedIncotermsDesc', 'intermedPaymentTerm', 'intermedPaymentTermDesc');
+            }
+            else {
+                $additionalfields = array('vendorAff');
+            }
+            $required_fields = array_merge($required_fields, $additionalfields);
+
+            if(!empty($data['intermedIncoterms']) && !empty($data['vendorIncoterms']) && $data['vendorIncoterms'] != $data['intermedIncoterms']) {
+                $required_fields[] = 'freight';
+            }
+            foreach($required_fields as $field) {
+                if(empty($data[$field])) {
+                    $this->errorcode = 2;
+                    return true;
+                }
+            }
+        }
+    }
+
+    private function calculate_partiesinfofields(array $data = array()) {
+        if(!$this->validate_requiredfields($data)) {
+            $fields = array('aorid', 'transitTime', 'clearanceTime', 'shipmentCountry', 'originCountry', 'freight', 'bankFees', 'insurance', 'legalization', 'courier', 'otherFees',
+                    'vendorEid', 'vendorIsAff', 'vendorAff', 'vendorIncoterms', 'vendorIncotermsDesc', 'vendorPaymentTerm', 'vendorPaymentTermDesc', 'commission', 'totalDiscount');
+            foreach($fields as $field) {
+                $partiesinfo_data[$field] = $data[$field];
+            }
+            $partiesinfo_data['estDateOfShipment'] = strtotime($data['estDateOfShipment']);
+            $partiesinfo_data['vendorEstDateOfPayment'] = $this->get_vendordates();
+            if($data['vendorIsAff'] !== 1) {
+                $additionalfields = array('intermedAff', 'intermedIncoterms', 'intermedIncotermsDesc', 'intermedPaymentTerm', 'intermedPaymentTermDesc', 'ptAcceptableMargin');
+                foreach($additionalfields as $field) {
+                    $partiesinfo_data[$field] = $data[$field];
+                }
+                $dates = $this->get_intermediarydates();
+                $partiesinfo_data['intermedEstDateOfPayment'] = $dates['intermedEstDateOfPayment'];
+                $partiesinfo_data['promiseOfPayment'] = $dates['promiseOfPayment'];
+            }
+        }
+        return $partiesinfo_data;
+    }
+
+    public function get_intermediarydates(array $data = array()) {
+        if(empty($data)) {
+            $data = $this->data;
+        }
+        $partiesinfo_data['estDateOfShipment'] = strtotime($data['estDateOfShipment']);
+        $intermed_paymentterm = PaymentTerms::get_data(array('ptid' => $data['intermedPaymentTerm']));
+        if(is_object($intermed_paymentterm)) {
+            $intermediarydates['intermedEstDateOfPayment'] = strtotime('+'.$intermed_paymentterm->overduePaymentDays.' days', $partiesinfo_data['estDateOfShipment']);
+        }
+        $intermediarydates['promiseOfPayment'] = strtotime('+'.$data['ptAcceptableMargin'].' days', $intermediarydates['intermedEstDateOfPayment']);
+        return $intermediarydates;
+    }
+
+    public function get_vendordates(array $data = array()) {
+        if(empty($data)) {
+            $data = $this->data;
+        }
+        $partiesinfo_data['estDateOfShipment'] = strtotime($data['estDateOfShipment']);
+        $vendor_paymentterm = PaymentTerms::get_data(array('ptid' => $data['vendorPaymentTerm']));
+        if(is_object($vendor_paymentterm)) {
+            $partiesinfo_data['vendorEstDateOfPayment'] = strtotime('+'.$vendor_paymentterm->overduePaymentDays.' days', $partiesinfo_data['estDateOfShipment']);
+        }
+        //  $partiesinfo_data['vendorEstDateOfPayment_formatted'] = date('d-m-Y', $partiesinfo_data['vendorEstDateOfPayment']);
+        return $partiesinfo_data['vendorEstDateOfPayment'];
+    }
+
+}
