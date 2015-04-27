cc211d5771142389538906c67e989e253473416d
unknown <hussein.barakat@L-PB4510-1.orkilalb.local>
2015-04-27 08:22:58 +0300

    Alter error message while deleting entities and functions

--- manage/modules/products/functions.php
+++ manage/modules/products/functions.php
@@ -125,7 +125,7 @@ elseif($core->input['action'] == 'deleteappfunc') {
     }
     if(is_array($usedin_tables)) {
         $result = implode(", ", $usedin_tables);
-        output_xml("<status>false</status><message>{$lang->deleteerror}<![CDATA[<br />{$lang->usedintables}{$result}]]></message>");
+        output_xml("<status>false</status><message>{$lang->deleteerror}</message>");
         exit;
     }
     /* Delete segappfunc */
