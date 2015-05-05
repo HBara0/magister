4b8330e207cc679af8bdf3640b12962b015dbbb4
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-05-05 12:00:56 +0300

    create basic ingredients db table

--- inc/db-schema/db-schema.sql
+++ inc/db-schema/db-schema.sql
@@ -266,6 +266,17 @@ CREATE TABLE `banks` (
   PRIMARY KEY (`bnkid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `basic_ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `basic_ingredients` (
+  `biid` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) NOT NULL,
+  `title` varchar(120) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  PRIMARY KEY (`biid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `budgeting_accountstrees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
