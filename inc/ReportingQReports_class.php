1b0430f7419858d25b507c17fd8684468b6ea703
zaher.reda <zaher.reda@orkila.com>
2015-05-28 17:13:19 +0300

    Add classes for Report Contributors and new class for QR

---
+++ inc/ReportingQReports_class.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Copyright © 2015 Orkila International Offshore, All Rights Reserved
+ *
+ * [Provide Short Descption Here]
+ * $id: ReportingQReports_class.php
+ * Created:        @zaher.reda    May 27, 2015 | 6:03:37 PM
+ * Last Update:    @zaher.reda    May 27, 2015 | 6:03:37 PM
+ */
+
+/**
+ * Description of ReportingQReports_class
+ *
+ * @author zaher.reda
+ */
+class ReportingQReports extends AbstractClass {
+    const PRIMARY_KEY = 'rid';
+    const TABLE_NAME = 'reports';
+    const DISPLAY_NAME = '';
+    const SIMPLEQ_ATTRS = '*';
+    const CLASSNAME = __CLASS__;
+    const UNIQUE_ATTRS = null;
+
+    protected function update(array $data) {
+
+    }
+
+    /**
+     *
+     * @return \Affiliates
+     */
+    public function get_affiliate() {
+        return new Affiliates($this->affid);
+    }
+
+}
