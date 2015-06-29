96d41ffd77e3732fb44bb52458b0bb62a64b5d96
hussein.barakat <hussein.barakat@orkila.com>
2015-06-29 11:30:05 +0300

    Fix filter issue in hotelslist page

--- modules/travelmanager.php
+++ modules/travelmanager.php
@@ -3,8 +3,8 @@ $module['name'] = 'travelmanager';
 $module['title'] = $lang->travelmanager;
 $module['homepage'] = 'listplans';
 $module['globalpermission'] = 'canUseTravelManager';
-$module['menu'] = array('file' => array('plantrip', 'listplans'),
-        'title' => array('plantrip', 'listplans'),
-        'permission' => array('canUseTravelManager', 'canUseTravelManager')
+$module['menu'] = array('file' => array('plantrip', 'listplans', 'hotelslist'),
+        'title' => array('plantrip', 'listplans', 'hotelslist'),
+        'permission' => array('canUseTravelManager', 'canUseTravelManager', 'canUseTravelManager')
 );
 ?>
