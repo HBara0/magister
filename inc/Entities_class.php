c45b8bafcfb0f04838e7011a1059e24a75f06656
hussein.barakat <hussein.barakat@orkila.com>
2015-08-03 12:56:56 +0300

    Add check on active suppliers in remindergrouppurchaseforecast cron

--- inc/Entities_class.php
+++ inc/Entities_class.php
@@ -677,7 +677,7 @@ class Entities extends AbstractClass {
         if(!empty($id)) {
             $query_select = '*';
             if($simple == true) {
-                $query_select = 'eid, companyName, companyName AS name, companyNameAbbr, companyNameShort, logo, country, type';
+                $query_select = 'eid, companyName, companyName AS name, companyNameAbbr, companyNameShort, logo, country, type,isActive';
             }
             return $db->fetch_assoc($db->query("SELECT ".$query_select." FROM ".Tprefix."entities WHERE eid='".$db->escape_string($id)."'"));
         }
