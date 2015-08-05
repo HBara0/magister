b8baac5aaafdafc7a20ccd590057fcc5097f48d5
hussein.barakat <hussein.barakat@orkila.com>
2015-08-05 12:06:15 +0300

    Alter saving method of translations

--- inc/Translations_class.php
+++ inc/Translations_class.php
@@ -60,4 +60,55 @@ class Translations extends AbstractClass {
         return false;
     }
 
+    public function save(array $data = array()) {
+        global $log;
+        if(empty($data)) {
+            $data = $this->data;
+        }
+
+        $log->record($this->data[static::PRIMARY_KEY], $data['inputChecksum']);
+
+        if(isset($this->data[static::PRIMARY_KEY]) && !empty($this->data[static::PRIMARY_KEY])) {
+            return $this->update($data);
+        }
+        if(empty($data[static::PRIMARY_KEY])) {
+            unset($data[static::PRIMARY_KEY]);
+        }
+
+        if(isset($data['inputChecksum']) && !empty($data['inputChecksum'])) {
+            $object = self::get_data(array('inputChecksum' => $data['inputChecksum']));
+            if(is_object($object)) {
+                return $object->update($data);
+            }
+        }
+        $language = new SystemLanguages($data['language']);
+        if(is_object($language) && !is_empty($language->htmllang)) {
+            $data['language'] = $language->htmllang;
+        }
+        $unique_attrs = explode(',', static::UNIQUE_ATTRS);
+        if(is_array($unique_attrs)) {
+            foreach($unique_attrs as $attr) {
+                $attr = trim($attr);
+                if(empty($data[$attr])) {
+                    $checks = null;
+                    break;
+                }
+                $checks[$attr] = $data[$attr];
+            }
+        }
+        if(is_array($checks)) {
+            $object = self::get_data($checks);
+            if(is_object($object)) {
+                return $object->update($data);
+            }
+
+            if(is_array($object)) {
+                foreach($object as $obj) {
+                    $obj->delete();
+                }
+            }
+        }
+        return $this->create($data);
+    }
+
 }
