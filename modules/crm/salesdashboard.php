ae505f9a3339e6e36e261f3d25532f65bbc15141
zaher.reda <zaher.reda@orkila.com>
2015-08-11 00:06:14 +0300

    Fix issues in sales report live and sales dashboard

--- modules/crm/salesdashboard.php
+++ modules/crm/salesdashboard.php
@@ -45,8 +45,9 @@ else {
         $query = $intgdb->query("SELECT totallines,dateinvoiced,c_invoice_id,c_currency_id FROM c_invoice WHERE issotrx='Y'AND docstatus='CO' AND c_invoice.ad_org_id IN ('".implode("','", $orgs)."') AND issotrx='Y' AND (dateinvoiced BETWEEN '".date('Y-m-d 00:00:00', strtotime((date('Y', TIME_NOW) - 2).'-01-01'))."' AND '".date('Y-m-d 00:00:00', strtotime((date('Y', TIME_NOW)).'-12-31'))."')");
         if($intgdb->num_rows($query) > 0) {
             while($line = $intgdb->fetch_assoc($query)) {
-                $currency_obj = IntegrationOBCurrency::get_data($line['c_currency_id']);
-                $lines['usdfxrate'] = $currency_obj->get_fxrate_bytype($core->input['fxtype'], $invoice->currency, array('from' => strtotime(date('Y-m-d', $invoice->dateinvoiceduts).' 01:00'), 'to' => strtotime(date('Y-m-d', $invoice->dateinvoiceduts).' 24:00'), 'year' => date('Y', $invoice->dateinvoiceduts), 'month' => date('m', $invoice->dateinvoiceduts)), array('precision' => 4));
+                // $obcurrency_obj = new IntegrationOBCurrency($line['c_currency_id']);
+                //$currency_obj = new Currencies($obcurrency_obj->cursymbol);
+                //$lines['usdfxrate'] = $currency_obj->get_fxrate_bytype('real', $invoice->currency, array('from' => strtotime(date('Y-m-d', $invoice->dateinvoiceduts).' 01:00'), 'to' => strtotime(date('Y-m-d', $invoice->dateinvoiceduts).' 24:00'), 'year' => date('Y', $invoice->dateinvoiceduts), 'month' => date('m', $invoice->dateinvoiceduts)), array('precision' => 4));
 
                 $invoice[dateinvoiceduts] = strtotime($line[dateinvoiced]);
                 $invoice[dateparts] = getdate($invoice[dateinvoiceduts]);
