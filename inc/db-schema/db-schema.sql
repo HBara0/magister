4a22077b56ff2fac3a66ea99964b746d5ef0711a
hussein.barakat <hussein.barakat@orkila.com>
2015-09-22 13:44:14 +0300

    Add last update time to attendance records table (DB CHANGE)

--- inc/db-schema/db-schema.sql
+++ inc/db-schema/db-schema.sql
@@ -505,6 +505,18 @@ CREATE TABLE `attendance_additionalleaves` (
   KEY `addedBy` (`addedBy`)
 ) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `attendance_attrecords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attendance_attrecords` (
+  `aarid` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL,
+  `time` bigint(30) NOT NULL,
+  `operation` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `lastupdateTime` bigint(30) NOT NULL,
+  PRIMARY KEY (`aarid`)
+) ENGINE=MyISAM AUTO_INCREMENT=77343 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `attendance_leaveexptypes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
