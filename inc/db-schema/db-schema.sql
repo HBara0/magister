ab33de2b3c17f3f55d59f5cc9144e27bc7d6d339
hussein.barakat <hussein.barakat@orkila.com>
2015-09-28 14:06:43 +0300

    Create facility reservations table (DB CHANGE)

--- inc/db-schema/db-schema.sql
+++ inc/db-schema/db-schema.sql
@@ -2343,6 +2343,20 @@ CREATE TABLE `facilitymgmt_features` (
   PRIMARY KEY (`fmftid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `facilitymgmt_reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `facilitymgmt_reservations` (
+  `fmrid` int(10) NOT NULL AUTO_INCREMENT,
+  `fmfid` int(10) NOT NULL,
+  `fromDate` int(30) NOT NULL,
+  `toDate` int(30) NOT NULL,
+  `reservedBy` int(10) NOT NULL,
+  `purpose` text NOT NULL,
+  `mtid` int(10) NOT NULL,
+  PRIMARY KEY (`fmrid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `files`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
