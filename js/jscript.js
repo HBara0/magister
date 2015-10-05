945f1d1c99f98b781b6ddf9d41ed9d86e96bd2b1
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-10-05 13:56:39 +0300

    Add lang variables

--- js/jscript.js
+++ js/jscript.js
@@ -459,9 +459,9 @@ $(function() {
             return;
         }
 
-        for(instance in CKEDITOR.instances) {
-            CKEDITOR.instances[instance].updateElement();
-        }
+        // for(instance in CKEDITOR.instances) {
+        //     CKEDITOR.instances[instance].updateElement();
+        //  }
 
         var id = $(this).attr("id").split("_");
         var formid = '';
