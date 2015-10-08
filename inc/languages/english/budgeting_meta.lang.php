994eef1fb553390ba95f77e29b3b96c460203de6
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-10-08 13:49:55 +0300

    Create copy budget template

--- inc/languages/english/budgeting_meta.lang.php
+++ inc/languages/english/budgeting_meta.lang.php
@@ -297,4 +297,7 @@ $lang['expiredstockdesc'] = 'Expired Stock';
 $lang['stockexpiringin60desc'] = 'Stock expiring within the next 60 days';
 $lang['oldstocknotexpireddesc'] = 'Oldest Stocks - not yet expired';
 $lang['stockpermonthofsalesdesc'] = 'Stock per month of sale';
+$lang['copybudget'] = 'Copy Budget';
+$lang['from'] = 'From';
+$lang['to'] = 'To';
 ?>
