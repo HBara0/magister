5acf5ff52a3a9e35dd3e473cd977ae6abf345997
hussein.barakat <hussein.barakat@orkila.com>
2015-11-02 09:39:42 +0200

    Show budgeting overview page in module list

--- modules/budgeting.php
+++ modules/budgeting.php
@@ -3,8 +3,8 @@ $module['name'] = 'budgeting';
 $module['title'] = $lang->budgeting;
 $module['homepage'] = 'budgethome';
 $module['globalpermission'] = 'canUseBudgeting';
-$module['menu'] = array('file' => array('createyearendforecast', 'generateyearendforecast', 'create', 'generate', 'createfinbudget', 'generatefinbudget', 'listfxrates', 'importbudget', 'massupdate', 'generatepresentation'),
-        'title' => array('createyearendforecast', 'generateyearendforecast', 'createbudget', 'generatebudget', 'fillfinancialbudget', 'generatefinbudget', 'listfxrates', 'importbudget', 'massupdate', 'generatepresentation'),
-        'permission' => array('budgeting_canFillBudget', 'budgeting_canFillBudget', 'budgeting_canFillBudget', 'canUseBudgeting', 'budgeting_canFillFinBudgets', 'budgeting_canFillFinBudgets', 'budgeting_canFillFinBudgets', 'canAdminCP', 'budgeting_canMassUpdate', 'canAdminCP')
+$module['menu'] = array('file' => array('budgethome', 'createyearendforecast', 'generateyearendforecast', 'create', 'generate', 'createfinbudget', 'generatefinbudget', 'listfxrates', 'importbudget', 'massupdate', 'generatepresentation'),
+        'title' => array('budgetingoverview', 'createyearendforecast', 'generateyearendforecast', 'createbudget', 'generatebudget', 'fillfinancialbudget', 'generatefinbudget', 'listfxrates', 'importbudget', 'massupdate', 'generatepresentation'),
+        'permission' => array('canUseBudgeting', 'budgeting_canFillBudget', 'budgeting_canFillBudget', 'budgeting_canFillBudget', 'canUseBudgeting', 'budgeting_canFillFinBudgets', 'budgeting_canFillFinBudgets', 'budgeting_canFillFinBudgets', 'canAdminCP', 'budgeting_canMassUpdate', 'canAdminCP')
 );
 ?>
