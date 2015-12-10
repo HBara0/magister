c3398496a1faf601e7e40d517956ca2cf9d9bb25
zaher.reda <zaher.reda@orkila.com>
2015-12-10 15:49:48 +0200

    Fix bug affecting leaves approval pipe in case leave has TM plan

--- pipes/approve_leaverequest.php
+++ pipes/approve_leaverequest.php
@@ -43,7 +43,7 @@ if(preg_match("/\[([a-zA-Z0-9]+)\]$/", $data['subject'], $subject) || $ignore_su
     if($db->num_rows($query) > 0) {
         $user = $db->fetch_assoc($query);
         //$user = $db->fetch_assoc($query);
-        $db->update_query('leavesapproval', array('isApproved' => 1, 'timeApproved' => TIME_NOW), "lid='{$leave[lid]}' AND uid='{$user[uid]}' AND isApproved='0'");
+        $db->update_query('leavesapproval', array('isApproved' => 1, 'timeApproved' => TIME_NOW), "lid='{$leave[lid]}' AND uid=".$user['uid']." AND isApproved='0'");
         if($db->affected_rows() > 0) {
             $query3 = $db->query("SELECT l.uid, u.email
 				FROM ".Tprefix."leavesapproval l LEFT JOIN ".Tprefix."users u ON (u.uid=l.uid)
@@ -271,7 +271,8 @@ if(preg_match("/\[([a-zA-Z0-9]+)\]$/", $data['subject'], $subject) || $ignore_su
                         $leave['type_details']['details_crumb'] = $core->sanitize_inputs($leave['type_details']['details_crumb'], array('method' => 'striponly', 'removetags' => true));
                         $lang->leavenotificationmessage_typedetails = $leave['type_details']['details_crumb'];
                     }
-                    else {
+
+                    if(empty($lang->leavenotificationmessage_typedetails)) {
                         $lang->leavenotificationmessage_typedetails = strtolower($leave['type_details']['title']);
                     }
 
@@ -303,7 +304,7 @@ if(preg_match("/\[([a-zA-Z0-9]+)\]$/", $data['subject'], $subject) || $ignore_su
                                     break;
                                 }
                             }
-                            $travelmanager_plan->email_finance($segment_expenses, sprint($lang->tmplanfinancenotification, $employee));
+                            $travelmanager_plan->email_finance($segment_expenses, $lang->sprint($lang->tmplanfinancenotification, $employee));
                         }
 
                         $log->record('notifyaffiliate', $mailingList);
