60d84154d97a02111e2e9b050ad805236d86f7d9
rasha.aboushakra <rasha.aboushakra@orkila.com>
2015-12-11 16:59:51 +0200

    Modify Aro Approval reminders

--- crons/aro_approvalreminder.php
+++ crons/aro_approvalreminder.php
@@ -22,16 +22,37 @@ if(is_array($arorequests)) {
         }
         $nextapproval = AroRequestsApprovals::get_data(array('sequence' => $sequence, 'aorid' => $arorequest->aorid));
         if(is_object($nextapproval) && TIME_NOW >= strtotime('+1 day', $nextapproval->emailRecievedDate)) {
-            $nextapprover = Users::get_data(array('uid' => $nextapproval->uid));
-            $aroaffiliate_obj = Affiliates::get_affiliates(array('affid' => $arorequest->affid));
-            $purchasteype_obj = PurchaseTypes::get_data(array('ptid' => $arorequest->orderType));
-            $approve_link = $core->settings['rootdir']."/index.php?module=aro/managearodouments&requestKey=".base64_encode($arorequest->data['identifier'])."&id=".$arorequest->aorid."&referrer=toapprove";
-            $approve_link = '<a href="'.$approve_link.'">Approve Link</a>';
+            $reminders[$nextapproval->uid][] = $arorequest;
+        }
+    }
+}
+
+if(is_array($reminders)) {
+    foreach($reminders as $uid => $userreminders) {
+        $nextapprover = Users::get_data(array('uid' => $uid));
+
+        if(is_array($userreminders)) {
+            $message = "Some AROs are pending your approval. Click on each link Go to the approval process of each following the below links: <br/>";
+            foreach($userreminders as $arorequest) {
+                if(!is_object($arorequest)) {
+                    continue;
+                }
+                $nextapproval = AroRequestsApprovals::get_data(array('aorid' => $arorequest->aorid, 'uid' => $uid));
+                $aroaffiliate_obj = Affiliates::get_affiliates(array('affid' => $arorequest->affid));
+                $purchasteype_obj = PurchaseTypes::get_data(array('ptid' => $arorequest->orderType));
+                $approve_link = $core->settings['rootdir']."/index.php?module=aro/managearodouments&requestKey=".base64_encode($arorequest->data['identifier'])."&id=".$arorequest->aorid."&referrer=toapprove";
+                $approve_link = '<a href="'.$approve_link.'">Aro Request ['.$arorequest->orderReference.'] '.$aroaffiliate_obj->get_displayname().' '.$purchasteype_obj->get_displayname().'</a>';
+                $message .= $approve_link;
+                if(!empty($nextapproval->emailRecievedDate)) {
+                    $message .= " recieved on ".date($core->settings['dateformat'], $nextapproval->emailRecievedDate);
+                }
+                $message .= '<br/>';
+            }
             $email_data = array(
                     'from' => 'ocos@orkila.com',
                     'to' => $nextapprover->email,
-                    'subject' => "ARO Request</span> [".$arorequest->orderReference."] ".$aroaffiliate_obj->get_displayname()." | ".$purchasteype_obj->get_displayname().' is pending your approval.',
-                    'message' => "Aro Request [".$arorequest->orderReference."] ".$aroaffiliate_obj->get_displayname()." ".$purchasteype_obj->get_displayname()." recieved on ".date($core->settings['dateformat'], $nextapproval->emailRecievedDate)." Needs Approval:".$approve_link,
+                    'subject' => "ARO Requests pending your approval",
+                    'message' => $message,
             );
 
             $mailer = new Mailer();
