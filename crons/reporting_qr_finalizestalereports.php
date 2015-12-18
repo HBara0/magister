01d79dfc5b05cd5d76e9cde66394126a1de91cd3
hussein.barakat <hussein.barakat@orkila.com>
2015-12-18 17:04:36 +0200

    Add record of QR status change (OCOS-667)

--- crons/reporting_qr_finalizestalereports.php
+++ crons/reporting_qr_finalizestalereports.php
@@ -10,23 +10,25 @@
 
 require_once '../inc/init.php';
 if($_REQUEST['authkey'] == 'kia5ravb$op09dj4a!xhegalhj') {
-    $log = new Log();
     $quarter = currentquarter_info();
     /* calculate the timestamp 7 days ago before NOW */
     $timelimit = TIME_NOW - (7 * 24 * 60 * 60);
-    $unfinalizedreports = ReportingQReports::get_data(array('year' => $quarter, 'modifiedOn' => $timelimit, 'type' => 'q', 'prActivityAvailable' => 1, 'mktReportAvailable' => 1, 'status' => 0, 'isSent' => 0, 'quarter' => $quarter['quarter']), array('operators' => array('modifiedOn' => 'lt'), 'returnarray' => true));
+    $unfinalizedreports = ReportingQReports::get_data(array('year' => $quarter['year'], 'modifiedOn' => $timelimit, 'type' => 'q', 'prActivityAvailable' => 1, 'mktReportAvailable' => 1, 'status' => 0, 'isSent' => 0, 'quarter' => $quarter['quarter']), array('operators' => array('modifiedOn' => 'lt'), 'returnarray' => true));
     if(is_array($unfinalizedreports)) {
         foreach($unfinalizedreports as $report) {
             $new_status = array(
                     'uidFinish' => 0,
                     'finishDate' => TIME_NOW,
                     'status' => 1,
-                    'prActivityAvailable' => 1,
-                    'keyCustAvailable' => 1,
-                    'mktReportAvailable' => 1,
                     'isLocked' => 1
             );
             $update_status = $db->update_query('reports', $new_status, "rid='{$report->rid}'");
+            $findata['newStatus'] = 1;
+            $findata['actionType'] = 'finalize';
+            $findata['rid'] = $rid;
+            $reportfinstatus = new ReportingFinalizeStatus();
+            $reportfinstatus->set($findata);
+            $reportfinstatus->save();
             $log->record('finalizeqr_bycron', $report->rid);
         }
     }
