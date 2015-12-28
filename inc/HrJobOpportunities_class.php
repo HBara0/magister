482b1c88a393906f189bb10e02d63384ba3e1bab
hussein.barakat <hussein.barakat@orkila.com>
2015-12-28 12:48:02 +0200

    Finish Create Job Opportunities Page (OCOS-298)

--- inc/HrJobOpportunities_class.php
+++ inc/HrJobOpportunities_class.php
@@ -33,11 +33,11 @@ class HrJobOpportunities extends AbstractClass {
         global $db, $log, $core, $errorhandler, $lang;
         if(!$this->validate_requiredfields($data)) {
             $this->errorcode = 1;
-            return false;
+            return $this;
         }
-        $dates = array('approxJoinDate', 'publishOn', 'unpublishOn', 'publishingTimeZone');
+        $dates = array('approxJoinDate', 'publishOn', 'unpublishOn');
         foreach($dates as $date) {
-            if(isset($data[$date]) && !empty($data[$date])) {
+            if(isset($data[$date]) && !empty($data[$date]) && !is_numeric($data[$date])) {
                 $data[$date] = strtotime($data[$date]);
             }
         }
@@ -55,19 +55,26 @@ class HrJobOpportunities extends AbstractClass {
         /* Verify if user can HR this affiliate Server side --START */
         if($core->usergroup['hr_canHrAllAffiliates'] == 0) {
             if(!in_array($data['affid'], $core->user['hraffids'])) {
-                return false;
+                return $this;
             }
         }
-        /* Verify if user can HR this affiliate Server side --END */
-
-        if(value_exists(self::TABLE_NAME, 'affid', $data['affid'], '(('.TIME_NOW.' BETWEEN '.$data['publishOn'].' AND '.$data['unpublishOn'].') AND title="'.$data['title'].'" )')) {
-            $this->errorcode = 4;
-            return false;
+        if($data['unpublishOn'] < $data['publishOn']) {
+            $this->errorcode = 5;
+            return $this;
         }
+        /* Verify if user can HR this affiliate Server side --END */
+//
+//        if(value_exists(self::TABLE_NAME, 'affid', $data['affid'], '(('.TIME_NOW.' BETWEEN '.$data['publishOn'].' AND '.$data['unpublishOn'].') AND title="'.$data['title'].'" )')) {
+//            $this->errorcode = 4;
+//            return $this;
+//        }
 
         $requiredlangs = $data['requiredlang'];
         unset($data['requiredlang']);
-
+        $requiredcareerlevel = $data['careerLevel'];
+        unset($data['careerLevel']);
+        $requirededucationlevel = $data['educationLevel'];
+        unset($data['educationLevel']);
         if(is_array($data)) {
             $query = $db->insert_query(self::TABLE_NAME, $data);
             if($query) {
@@ -75,11 +82,38 @@ class HrJobOpportunities extends AbstractClass {
                 $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
                 if(!empty($requiredlangs) && is_array($requiredlangs)) {
                     $langdata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                    $langdata['type'] = 'requirement';
                     foreach($requiredlangs as $langid) {
                         $hrjoblang = new HrJobOpportunitiesLanguage();
                         $langdata['language'] = $langid;
                         $hrjoblang->set($langdata);
                         $hrjoblang->save();
+                    }
+                }
+                if(!empty($requiredcareerlevel) && is_array($requiredcareerlevel)) {
+                    $leveldata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                    $leveldata['type'] = 'requirement';
+                    foreach($requiredcareerlevel as $levelid) {
+                        if($levelid == 0) {
+                            continue;
+                        }
+                        $hrjobcareerlevel = new HrJobOpportunitiesSelectedCareerLevel();
+                        $leveldata['joclid'] = $levelid;
+                        $hrjobcareerlevel->set($leveldata);
+                        $hrjobcareerlevel->save();
+                    }
+                }
+                if(!empty($requirededucationlevel) && is_array($requirededucationlevel)) {
+                    $leveldata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                    $leveldata['type'] = 'requirement';
+                    foreach($requirededucationlevel as $levelid) {
+                        if($levelid == 0) {
+                            continue;
+                        }
+                        $hrjobeducationlevel = new HrJobOpportunitiesSelectedEducationLevel();
+                        $leveldata['joelid'] = $levelid;
+                        $hrjobeducationlevel->set($leveldata);
+                        $hrjobeducationlevel->save();
                     }
                 }
             }
@@ -88,7 +122,89 @@ class HrJobOpportunities extends AbstractClass {
     }
 
     protected function update(array $data) {
+        global $db, $log, $core, $errorhandler, $lang;
+        if(!$this->validate_requiredfields($data)) {
+            $this->errorcode = 1;
+            return $this;
+        }
+        $dates = array('approxJoinDate', 'publishOn', 'unpublishOn');
+        foreach($dates as $date) {
+            if(isset($data[$date]) && !empty($data[$date]) && !is_numeric($data[$date])) {
+                $data[$date] = strtotime($data[$date]);
+            }
+        }
+        $data['modifiedOn'] = TIME_NOW;
+        $data['modifiedBy'] = $core->user['uid'];
 
+        /* ---SANITIZE INPUTS---START */
+        $sanitize_fields = array('reference', 'title', 'shortDesc', 'responsibilities', 'minQualifications', 'prefQualifications');
+        foreach($sanitize_fields as $val) {
+            $data[$val] = $core->sanitize_inputs($data[$val], array('removetags' => true));
+        }
+        /* ---SANITIZE INPUTS---END */
+
+
+        /* Verify if user can HR this affiliate Server side --START */
+        if($core->usergroup['hr_canHrAllAffiliates'] == 0) {
+            if(!in_array($data['affid'], $core->user['hraffids'])) {
+                return $this;
+            }
+        }
+
+        if($data['unpublishOn'] < $data['publishOn']) {
+            $this->errorcode = 5;
+            return $this;
+        }
+        $requiredlangs = $data['requiredlang'];
+        unset($data['requiredlang']);
+        $requiredcareerlevel = $data['careerLevel'];
+        unset($data['careerLevel']);
+        $requirededucationlevel = $data['educationLevel'];
+        unset($data['educationLevel']);
+        if(is_array($data)) {
+            $db->update_query(self::TABLE_NAME, $data, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]));
+            $log->record(self::TABLE_NAME, $this->data[self::PRIMARY_KEY]);
+            $query = $db->delete_query(HrJobOpportunitiesLanguage::TABLE_NAME, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]));
+            if(!empty($requiredlangs) && is_array($requiredlangs)) {
+                $langdata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                $langdata['type'] = 'requirement';
+                foreach($requiredlangs as $langid) {
+                    $hrjoblang = new HrJobOpportunitiesLanguage();
+                    $langdata['language'] = $langid;
+                    $hrjoblang->set($langdata);
+                    $hrjoblang->save();
+                }
+            }
+            $query1 = $db->delete_query(HrJobOpportunitiesSelectedCareerLevel::TABLE_NAME, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]).' AND type="requirement"');
+            if(!empty($requiredcareerlevel) && is_array($requiredcareerlevel)) {
+                $leveldata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                $leveldata['type'] = 'requirement';
+                foreach($requiredcareerlevel as $levelid) {
+                    if($levelid == 0) {
+                        continue;
+                    }
+                    $hrjobcareerlevel = new HrJobOpportunitiesSelectedCareerLevel();
+                    $leveldata['joclid'] = $levelid;
+                    $hrjobcareerlevel->set($leveldata);
+                    $hrjobcareerlevel->save();
+                }
+            }
+            $query2 = $db->delete_query(HrJobOpportunitiesSelectedEducationLevel::TABLE_NAME, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]).' AND type="requirement"');
+            if(!empty($requirededucationlevel) && is_array($requirededucationlevel)) {
+                $leveldata[self::PRIMARY_KEY] = $this->data[self::PRIMARY_KEY];
+                $leveldata['type'] = 'requirement';
+                foreach($requirededucationlevel as $levelid) {
+                    if($levelid == 0) {
+                        continue;
+                    }
+                    $hrjobeducationlevel = new HrJobOpportunitiesSelectedEducationLevel();
+                    $leveldata['joelid'] = $levelid;
+                    $hrjobeducationlevel->set($leveldata);
+                    $hrjobeducationlevel->save();
+                }
+            }
+        }
+        return $this;
     }
 
 }
