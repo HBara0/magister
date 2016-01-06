20986056d0ee282eb7401d7e277c644682529006
hussein.barakat <hussein.barakat@orkila.com>
2016-01-06 12:45:25 +0200

    Minor change in users query in users_filltokens cron

--- crons/users_filltokens.php
+++ crons/users_filltokens.php
@@ -11,7 +11,7 @@
 require_once '../inc/init.php';
 
 //if($_REQUEST['authkey'] == 'asfasdkjj!h4k23jh4k2_3h4k23jh') {
-$allactiveusers = Users::get_data('gid !=7 AND (token IS NULL OR token = "") ', array('returnarray' => true, 'simple' => false));
+$allactiveusers = Users::get_data('gid !=7 AND (apiKey IS NULL OR apiKey = "") ', array('returnarray' => true, 'simple' => false));
 if(is_array($allactiveusers)) {
     foreach($allactiveusers as $user) {
         $user->save_apikey();
