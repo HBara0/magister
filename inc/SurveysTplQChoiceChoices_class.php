7c88bd69e582fc7c3c09f5f20c5b5c0e2b2a06d6
hussein.barakat <hussein.barakat@orkila.com>
2016-01-19 14:23:46 +0200

    Show charts for survey matrix questions and Fix parsing of matrix questions (OCOS-773 OCOS-749)

--- inc/SurveysTplQChoiceChoices_class.php
+++ inc/SurveysTplQChoiceChoices_class.php
@@ -1,30 +1,30 @@
 <?php
-/*-------Definiton-START--------*/
+/* -------Definiton-START-------- */
+
 class SurveysTplQChoiceChoices extends AbstractClass {
-        protected $data = array();
-        protected $errorcode = 0;
-        const PRIMARY_KEY = 'stqccid';
-        const TABLE_NAME = 'surveys_templates_questionschoices_choices';
-        const SIMPLEQ_ATTRS = '*';
-        const UNIQUE_ATTRS = '';
-        const CLASSNAME = __CLASS__;
-        const DISPLAY_NAME = '';
+    protected $data = array();
+    protected $errorcode = 0;
 
-                    /*-------Definiton-END--------*/
-/*-------FUNCTIONS-START--------*/
+    const PRIMARY_KEY = 'stqccid';
+    const TABLE_NAME = 'surveys_templates_questionschoices_choices';
+    const SIMPLEQ_ATTRS = '*';
+    const UNIQUE_ATTRS = '';
+    const CLASSNAME = __CLASS__;
+    const DISPLAY_NAME = 'choice';
 
-public function __construct($id = '', $simple = true) {
+    /* -------Definiton-END-------- */
+    /* -------FUNCTIONS-START-------- */
+    public function __construct($id = '', $simple = true) {
         parent::__construct($id, $simple);
-                }
+    }
 
-public function create(array $data) {
-        global $db,$core;
+    public function create(array $data) {
+        global $db, $core;
         $table_array = array(
- 	'stqcid' => $data['stqcid'],
-	'choice' => $data['choice'],
-	'value' => $data['value'],
-
-                );
+                'stqcid' => $data['stqcid'],
+                'choice' => $data['choice'],
+                'value' => $data['value'],
+        );
         $query = $db->insert_query(self::TABLE_NAME, $table_array);
         if($query) {
             $this->data[self::PRIMARY_KEY] = $db->last_id();
@@ -32,18 +32,16 @@ public function create(array $data) {
         return $this;
     }
 
-protected function update(array $data) {
+    protected function update(array $data) {
         global $db;
         if(is_array($data)) {
-	$update_array['stqcid']=$data['stqcid'];
-	$update_array['choice']=$data['choice'];
-	$update_array['value']=$data['value'];
-
-                    }
-       $db->update_query(self::TABLE_NAME, $update_array, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]));
-        return $this;
+            $update_array['stqcid'] = $data['stqcid'];
+            $update_array['choice'] = $data['choice'];
+            $update_array['value'] = $data['value'];
         }
+        $db->update_query(self::TABLE_NAME, $update_array, self::PRIMARY_KEY.'='.intval($this->data[self::PRIMARY_KEY]));
+        return $this;
+    }
 
-/*-------FUNCTIONS-END--------*/
-
+    /* -------FUNCTIONS-END-------- */
 }
