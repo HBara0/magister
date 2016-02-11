0e9f643f159ffed5b53db86b03e3e92bac8c92ed
rasha.aboushakra <rasha.aboushakra@orkila.com>
2016-02-11 17:23:18 +0200

    Fix bug in REvoke leave base64 decode for leave id

--- modules/attendance/listleaves.php
+++ modules/attendance/listleaves.php
@@ -210,9 +210,8 @@ if(!$core->input['action']) {
 }
 else {
     if($core->input['action'] == 'perform_revokeleave') {
-        $lid = intval($core->input['torevoke']);
+        $lid = intval(base64_decode($core->input['torevoke']));
         $user_obj = new Users($core->user['uid']);
-
         $leave_obj = new Leaves($lid, false);
         $leave = $leave_obj->get();
         $leave_type = $leave_obj->get_leavetype(false)->get();
