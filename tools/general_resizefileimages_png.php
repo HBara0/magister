4f02f6329749fe456dca08f803cde209cb73d5a7
hussein.barakat <hussein.barakat@orkila.com>
2016-03-30 11:58:23 +0300

    Create function to resize all png images in a file

---
+++ tools/general_resizefileimages_png.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright © 2016 Orkila International Offshore, All Rights Reserved
+ *
+ * [Provide Short Descption Here]
+ * $id: general_resizefileimages.php
+ * Created:        @hussein.barakat    30-Mar-2016 | 09:48:15
+ * Last Update:    @hussein.barakat    30-Mar-2016 | 09:48:15
+ */
+
+require '../inc/init.php';
+if($_REQUEST['authkey'] == 'kia5ravb$op09dj4a!xhegalhj') {
+    if(!empty($core->input['file'])) {
+        iteratethrough_file('C:\\www\\development\\ocos\\'.$core->input['file'], 'C:\\www\\development\\ocos\\'.$core->input['file'].'\\newimgs');
+    }
+}
+/**
+ *
+ * @param type $file
+ */
+function iteratethrough_file($file, $outfile = '') {
+    $dir = new DirectoryIterator($file);
+    foreach($dir as $fileinfo) {
+        if(!$fileinfo->isDot()) {
+            if($fileinfo->isDir()) {
+                iteratethrough_file($fileinfo->getRealPath());
+            }
+            else {
+                if($fileinfo->getExtension() == 'png') {
+                    if($outfile) {
+                        $outfile_path = $outfile;
+                    }
+                    else {
+                        $outfile_path = dirname($file);
+                    }
+                    resize_image_png($fileinfo->getRealPath(), 200, 200, $outfile_path.'\\newimgs\\'.$fileinfo->getBasename());
+                    echo($fileinfo->getFilename().'<br>');
+                }
+            }
+        }
+    }
+    return;
+}
