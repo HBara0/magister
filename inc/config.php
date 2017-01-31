f1e1456d1031354c6370df802d1c2ab0b7cafaf5
hussein.barakat <hbbarakat1@hotmail.com>
2017-01-31 11:32:36 +0100

    Change database name

--- inc/config.php
+++ inc/config.php
@@ -6,7 +6,7 @@
 $config['database']['hostname'] = 'localhost';
 $config['database']['username'] = 'root';
 $config['database']['password'] = '';
-$config['database']['database'] = 'magister';
+$config['database']['database'] = 'motis2017_1';
 $config['database']['prefix'] = '';
 $config['database']['encoding'] = 'utf8';
 ?>
